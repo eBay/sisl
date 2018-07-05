05df51e311f11b4fb5b4a589f6c8464e78d1cdeb
Brian Szmyd <bszmyd@ebay.com>
2018-07-05 17:12:22 +0000

    Static module levels

--- logging/sds_logging/logging.h
+++ logging/sds_logging/logging.h
@@ -9,6 +9,10 @@
 #include <unordered_map>
 #include <spdlog/spdlog.h>
 
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/variadic/to_seq.hpp>
+
 #pragma once
 
 // The following constexpr's are used to extract the filename
@@ -28,31 +32,37 @@ constexpr const char* file_name(const char* str) {
     return str_slant(str) ? r_slant(str_end(str)) : str;
 }
 
+#define LEVELCHECK(l, mod, lvl) (l && sds_logging::module_level_##mod <= (lvl))
 
 #define LINEOUTPUTFORMAT "[{}:{}:{}] "
 #define LINEOUTPUTARGS file_name(__FILE__), __LINE__, __FUNCTION__
-
-#define LEVELCHECK(l, lvl) (l && l->should_log(lvl))
-
 #define LOGGER (sds_logging::sds_thread_logger ? sds_logging::sds_thread_logger : sds_logging::sds_thread_logger = sds_logging::GetLogger())
 
-#define LOGTRACE(msg, ...)      if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::trace)) _l->trace(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGDEBUG(msg, ...)      if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::debug)) _l->debug(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGINFO(msg, ...)       if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::info)) _l->info(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGWARN(msg, ...)       if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::warn)) _l->warn(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGERROR(msg, ...)      if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::err)) _l->error(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGCRITICAL(msg, ...)   if (auto _l = LOGGER; _l && LEVELCHECK(_l, spdlog::level::level_enum::critical)) _l->critical(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGTRACEMOD(mod, msg, ...)      if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::trace)) \
+                                            _l->trace(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGDEBUGMOD(mod, msg, ...)      if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::debug)) \
+                                            _l->debug(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGINFOMOD(mod, msg, ...)       if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::info)) \
+                                            _l->info(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGWARNMOD(mod, msg, ...)       if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::warn)) \
+                                            _l->warn(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGERRORMOD(mod, msg, ...)      if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::err)) \
+                                            _l->error(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGCRITICALMOD(mod, msg, ...)   if (auto _l = LOGGER; \
+                                                _l && LEVELCHECK(_l, mod, spdlog::level::level_enum::critical)) \
+                                            _l->critical(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
 
-#define MODSTRINGIFY(X) MODSTRINGY(X)
-#define MODSTRINGY(X) #X
-#define MODLOGGER(mod, lvl) sds_logging::GetLogger(MODSTRINGIFY(mod), lvl)
-
-#define LOGTRACEMOD(mod, msg, ...)     if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::trace); _l) _l->trace(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGDEBUGMOD(mod, msg, ...)     if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::debug); _l) _l->debug(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGINFOMOD(mod, msg, ...)      if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::info); _l) _l->info(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGWARNMOD(mod, msg, ...)      if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::warn); _l) _l->warn(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGERRORMOD(mod, msg, ...)     if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::err); _l) _l->error(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
-#define LOGCRITICALMOD(mod, msg, ...)  if (auto _l = MODLOGGER(mod,spdlog::level::level_enum::critical); _l) _l->critical(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__)
+#define LOGTRACE(msg, ...)      LOGTRACEMOD(base, msg, ##__VA_ARGS__)
+#define LOGDEBUG(msg, ...)      LOGDEBUGMOD(base, msg, ##__VA_ARGS__)
+#define LOGINFO(msg, ...)       LOGINFOMOD(base, msg, ##__VA_ARGS__)
+#define LOGWARN(msg, ...)       LOGWARNMOD(base, msg, ##__VA_ARGS__)
+#define LOGERROR(msg, ...)      LOGERRORMOD(base, msg, ##__VA_ARGS__)
+#define LOGCRITICAL(msg, ...)   LOGCRITICALMOD(base, msg, ##__VA_ARGS__)
 
 namespace sds_logging {
 template <typename T>
@@ -60,48 +70,41 @@ using shared = std::shared_ptr<T>;
 
 extern thread_local shared<spdlog::logger> sds_thread_logger;
 extern shared<spdlog::logger> GetLogger() __attribute__((weak));
-extern shared<spdlog::logger> GetLogger(std::string const& name, spdlog::level::level_enum const lvl) __attribute__((weak));
-extern void EnableModule(std::string const& name) __attribute__((weak));
-extern void SetLogger(shared<spdlog::logger>) __attribute__((weak));
 }
 
-#define SDS_LOGGING_INIT                                            \
-   static std::shared_ptr<spdlog::logger> logger_;                  \
-                                                                    \
-   namespace sds_logging {                                          \
-   thread_local shared<spdlog::logger> sds_thread_logger;           \
-   thread_local std::unordered_map<std::string,                     \
-                            shared<spdlog::logger>> module_loggers; \
-                                                                    \
-   void EnableModule(std::string const& n) {                        \
-       assert(logger_);                                             \
-       auto const& sinks = logger_->sinks();                        \
-       auto l = std::make_shared<spdlog::logger>(n,                 \
-                                                 sinks.begin(),     \
-                                                 sinks.end());      \
-       l->set_level(logger_->level());                              \
-       spdlog::register_logger(l);                                  \
-   }                                                                \
-                                                                    \
-   shared<spdlog::logger> GetLogger() {                             \
-       return logger_;                                              \
-   }                                                                \
-                                                                    \
-   shared<spdlog::logger> GetLogger(std::string const& name,        \
-                                    spdlog::level::level_enum const lvl) { \
-       if (!LEVELCHECK(LOGGER, lvl)) return nullptr;                \
-       if (auto it = module_loggers.find(name);                     \
-              module_loggers.end() != it) {                         \
-           return it->second;                                       \
-       }                                                            \
-       auto logger = spdlog::get(name) ;                            \
-       module_loggers.emplace(std::make_pair(name, logger));        \
-       return logger;                                               \
-   }                                                                \
-                                                                    \
-   void SetLogger(std::shared_ptr<spdlog::logger> logger) {         \
-       logger_ = logger; sds_thread_logger = logger;                \
-   }                                                                \
+#define MODLEVELDEC(r, _, module) \
+    namespace sds_logging { \
+        extern spdlog::level::level_enum BOOST_PP_CAT(module_level_, module); \
+    }
+MODLEVELDEC(_, _, base)
+
+#define MODLEVELDEF(r, l, module) \
+    namespace sds_logging { \
+        spdlog::level::level_enum BOOST_PP_CAT(module_level_, module) {l}; \
+    }
+
+#define SDS_LOGGING_DECL(...)                                                   \
+   BOOST_PP_SEQ_FOR_EACH(MODLEVELDEC, spdlog::level::level_enum::off, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+
+#define SDS_LOGGING_INIT(...)                                                   \
+   static std::shared_ptr<spdlog::logger> logger_;                              \
+                                                                                \
+   BOOST_PP_SEQ_FOR_EACH(MODLEVELDEF, spdlog::level::level_enum::off, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)) \
+   namespace sds_logging {                                                      \
+   thread_local shared<spdlog::logger> sds_thread_logger;                       \
+                                                                                \
+   shared<spdlog::logger> GetLogger() {                                         \
+       return logger_;                                                          \
+   }                                                                            \
+                                                                                \
+   void SetLogger(shared<spdlog::logger> logger,                                \
+                  spdlog::level::level_enum const lvl = spdlog::level::level_enum::off) { \
+       if (logger) {                                                            \
+            logger->set_level(spdlog::level::level_enum::trace);                \
+            module_level_base = lvl;                                            \
+       }                                                                        \
+       logger_ = logger; sds_thread_logger = logger;                            \
+   }                                                                            \
    }
 
-#define LOGENABLEMOD(module) sds_logging::EnableModule(MODSTRINGIFY(module));
+#define SDS_LOG_LEVEL(mod, lvl) BOOST_PP_CAT(sds_logging::module_level_, mod) = (lvl);
