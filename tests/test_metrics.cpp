68e897869320cd6a72b9a37fb5858b7a25d16134
soungupta <soungupta@ebay.com>
2018-08-01 17:47:24 -0700

    modified the test script to enable unregistering of rcu threads

--- tests/test_metrics.cpp
+++ tests/test_metrics.cpp
@@ -3,30 +3,39 @@
 
 class TestMetrics {
 public:
-    TestMetrics() = default;
-    metrics::ReportMetrics getMetrics() { return m_report_metrics; }
+    TestMetrics() {
+        m_report_metrics = new metrics::ReportMetrics();
+    }
+
+    ~TestMetrics() {
+        delete m_report_metrics;
+    }
+
+    metrics::ReportMetrics* getMetrics() { return m_report_metrics; }
 
 private:
-    metrics::ReportMetrics m_report_metrics;
+    metrics::ReportMetrics* m_report_metrics;
 };
 
 int main() {
-    TestMetrics report;
-    auto a_cindx = report.getMetrics().registerCounter( "counter1", "counter for test1", "", 0 );
-    auto a_gindx = report.getMetrics().registerGauge( "gauge1", "gauge for test2", "", 3 );
+    TestMetrics *report = new TestMetrics();
+    auto a_cindx = report->getMetrics()->registerCounter( "counter1", "counter for test1", "", 0 );
+    auto a_gindx = report->getMetrics()->registerGauge( "gauge1", "gauge for test2", "", 3 );
 
-    report.getMetrics().getCounter(a_cindx).increment();
-    report.getMetrics().getCounter(a_cindx).increment();
+    report->getMetrics()->getCounter(a_cindx).increment();
+    report->getMetrics()->getCounter(a_cindx).increment();
 
-    report.getMetrics().getGauge(a_gindx).update(2);
+    report->getMetrics()->getGauge(a_gindx).update(2);
 
-    report.getMetrics().getCounter(a_cindx).decrement();
+    report->getMetrics()->getCounter(a_cindx).decrement();
 
-    report.getMetrics().gather();
+    report->getMetrics()->gather();
 
     std::ofstream ofs ("test.txt", std::ofstream::out);
-    ofs << report.getMetrics().getJSON();
+    ofs << report->getMetrics()->getJSON();
     ofs.close();
+
+    delete report;
 
     return 0;
 }
