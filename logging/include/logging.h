d938c25a00796152461d5cc560e30c95445fb960
Hari Kadayam <hkadayam@ebay.com>
2019-01-17 17:23:59 -0800

    SDSTOR-797 LOGDFATAL_IF to log and assert in debug build

--- logging/include/logging.h
+++ logging/include/logging.h
@@ -82,6 +82,21 @@ constexpr const char* file_name(const char* str) {
 #define LOGERROR(msg, ...)      LOGERRORMOD(base, msg, ##__VA_ARGS__)
 #define LOGCRITICAL(msg, ...)   LOGCRITICALMOD(base, msg, ##__VA_ARGS__)
 
+#define LOGCRITICAL_AND_FLUSH(msg, ...) {auto _l = LOGGER; _l->critical(LINEOUTPUTFORMAT msg, LINEOUTPUTARGS, ##__VA_ARGS__); _l->flush(); }
+
+#ifndef DEBUG
+#define LOGDFATAL_IF(cond, msg, ...) \
+	if (cond) { \
+		LOGCRITICAL_AND_FLUSH(msg, __VA_ARGS__); \
+		assert(0); \
+	}
+#else
+#define LOGDFATAL_IF(cond, msg, ...) \
+	if (cond) { \
+		LOGCRITICAL_AND_FLUSH(msg, __VA_ARGS__); \
+	}
+#endif
+
 namespace sds_logging {
 template <typename T>
 using shared = std::shared_ptr<T>;
