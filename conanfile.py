57c72c3d8107e468cb10469703407cf1d26ef79d
Hari Kadayam <hkadayam@ebay.com>
2019-01-29 19:53:32 -0800

    SDSTOR-872 Fixed potential race condition for metrics registration and scrapping, allow group level gather

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class MetricsConan(ConanFile):
     name = "sisl"
-    version = "0.2.0"
+    version = "0.2.1"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/Symbiosis/sisl"
