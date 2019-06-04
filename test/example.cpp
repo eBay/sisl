e711fdd64e8fa5349a3a85108003686d08588347
Hari Kadayam <harihara.kadayam@gmail.com>
2019-06-04 13:22:36 -0700

    Log critical messages to separate file apart from async logger

--- test/example.cpp
+++ test/example.cpp
@@ -1,7 +1,7 @@
 #include <condition_variable>
 #include <iostream>
-#include <mutex>
 #include <logging.h>
+#include <mutex>
 //#include <stack_trace.h>
 #include <sds_options/options.h>
 
@@ -9,42 +9,41 @@ SDS_LOGGING_INIT(my_module)
 
 SDS_OPTIONS_ENABLE(logging)
 
-//SDS_LOG_LEVEL(my_module, spdlog::level::level_enum::error)
+// SDS_LOG_LEVEL(my_module, spdlog::level::level_enum::error)
 
 void func() {
-        LOGINFO("Thread func started");
-        auto i = 0;
-        while (i < 3) {
-                LOGINFO("Thread func {}th iteration", i+1);
-                sleep(3);
-                ++i;
-        }
+    LOGINFO("Thread func started");
+    auto i = 0;
+    while (i < 3) {
+        LOGINFO("Thread func {}th iteration", i + 1);
+        sleep(3);
+        ++i;
+    }
 }
 
 int main(int argc, char** argv) {
-   SDS_OPTIONS_LOAD(argc, argv, logging)
-   sds_logging::SetLogger(std::string(argv[0]));
-   spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
+    SDS_OPTIONS_LOAD(argc, argv, logging)
+    sds_logging::SetLogger(std::string(argv[0]));
+    spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
 
-   SDS_LOG_LEVEL(base, spdlog::level::level_enum::trace);
-   sds_logging::install_crash_handler();
+    SDS_LOG_LEVEL(base, spdlog::level::level_enum::trace);
+    sds_logging::install_crash_handler();
 
-   std::thread t(func);
-   sleep(1);
-   std::cout << "spdlog level base = " << module_level_base << "\n";
-   LOGTRACE("Trace");
-   LOGDEBUG("Debug");
-   LOGINFO("Info");
-   LOGWARN("Warning");
-   LOGERROR("Error");
-   LOGCRITICAL("Critical");
+    std::thread t(func);
+    sleep(1);
+    std::cout << "spdlog level base = " << module_level_base << "\n";
+    LOGTRACE("Trace");
+    LOGDEBUG("Debug");
+    LOGINFO("Info");
+    LOGWARN("Warning");
+    LOGERROR("Error");
+    LOGCRITICAL("Critical");
 
-   SDS_LOG_LEVEL(my_module, spdlog::level::level_enum::info);
-   LOGINFOMOD(my_module, "Enabled Module Logger");
-   LOGTRACEMOD(my_module, "Trace Module");
+    SDS_LOG_LEVEL(my_module, spdlog::level::level_enum::info);
+    LOGINFOMOD(my_module, "Enabled Module Logger");
+    LOGTRACEMOD(my_module, "Trace Module");
 
-   //backtrace_unwind();
-   sds_logging::log_stack_trace();
+    sds_logging::log_stack_trace();
 
 #if 0
    RELEASE_ASSERT_EQ(argc, 2, "I can't run without proper arguments in release build");
@@ -54,6 +53,6 @@ int main(int argc, char** argv) {
    int* x = nullptr; *x = 5;
 #endif
 
-   t.join();
-   return 0;
+    t.join();
+    return 0;
 }
