f9a66e54e1de51405d717b84b3d38c7f540dca79
Hari Kadayam <harihara.kadayam@gmail.com>
2019-07-19 09:00:07 +0530

    Fixed the crash when a new thread is created in between make call and exchange call

--- src/utility/urcu_helper.hpp
+++ src/utility/urcu_helper.hpp
@@ -14,33 +14,31 @@
 #include <mutex>
 
 namespace sisl {
-template <typename T>
+template < typename T >
 struct urcu_node {
-    rcu_head head;
+    rcu_head             head;
     std::shared_ptr< T > val;
 
-    template <typename... Args>
+    template < typename... Args >
     urcu_node(Args&&... args) {
         val = std::make_shared< T >(std::forward< Args >(args)...);
     }
 
-    std::shared_ptr< T > get() {
-        return val;
-    }
+    std::shared_ptr< T > get() { return val; }
 
-    void set(const T &v) { val = v;}
+    void        set(const T& v) { val = v; }
     static void free(struct rcu_head* rh) {
         auto* node = (urcu_node*)rh;
         delete node;
     }
 };
 
-template <typename T>
+template < typename T >
 class urcu_ptr {
 public:
-    urcu_node<T>* m_gp;
+    urcu_node< T >* m_gp;
 
-    urcu_ptr(urcu_node<T>* gp) : m_gp(gp) { rcu_read_lock(); }
+    urcu_ptr(urcu_node< T >* gp) : m_gp(gp) { rcu_read_lock(); }
     ~urcu_ptr() { rcu_read_unlock(); }
 
     urcu_ptr(const urcu_ptr& other) = delete;
@@ -58,61 +56,65 @@ public:
     }
 };
 
-template <typename T>
+template < typename T >
 class urcu_data {
 public:
-
-    template< typename... Args>
+    template < typename... Args >
     urcu_data(Args&&... args) {
-        auto node = new urcu_node<T>(std::forward< Args >(args)...);
+        auto node = new urcu_node< T >(std::forward< Args >(args)...);
         rcu_assign_pointer(m_rcu_node, node);
         m_old_node = nullptr;
     }
 
     ~urcu_data() {
-        delete(m_rcu_node);
-        if (m_old_node != nullptr) { delete(m_old_node); }
+        delete (m_rcu_node);
+        if (m_old_node != nullptr) {
+            delete (m_old_node);
+        }
     }
 
-    template <typename... Args>
+    template < typename... Args >
     std::shared_ptr< T > make_and_exchange(Args&&... args) {
-        auto new_node = new urcu_node<T>(std::forward< Args >(args)...);
+        auto new_node = new urcu_node< T >(std::forward< Args >(args)...);
         auto old_rcu_node = m_rcu_node;
         rcu_assign_pointer(m_rcu_node, new_node);
         synchronize_rcu();
         auto ret = old_rcu_node->get();
-        call_rcu(&old_rcu_node->head, urcu_node<T>::free);
+        call_rcu(&old_rcu_node->head, urcu_node< T >::free);
         return ret;
     }
 
-    template <typename... Args>
+    template < typename... Args >
     void make(Args&&... args) {
-        auto new_node = new urcu_node<T>(std::forward< Args >(args)...);
+        auto new_node = new urcu_node< T >(std::forward< Args >(args)...);
         m_old_node = m_rcu_node;
         rcu_assign_pointer(m_rcu_node, new_node);
     }
 
     std::shared_ptr< T > exchange() {
+        if (m_old_node == nullptr) {
+            /* exchange called without make for this thread. Its possible if a new thread is spawned between make and
+             * exchange. Just return nullptr for this case */
+            return nullptr;
+        }
         auto ret = m_old_node->get();
-        call_rcu(&m_old_node->head, urcu_node<T>::free);
+        call_rcu(&m_old_node->head, urcu_node< T >::free);
         m_old_node = nullptr;
         return ret;
     }
 
-    urcu_ptr<T> get() const {
+    urcu_ptr< T > get() const {
         assert(m_rcu_node != nullptr);
-        return urcu_ptr<T>(m_rcu_node);
+        return urcu_ptr< T >(m_rcu_node);
     }
 
-    urcu_node<T> *get_node() const {
-        return m_rcu_node;
-    }
+    urcu_node< T >* get_node() const { return m_rcu_node; }
 
-    urcu_node<T>* m_rcu_node;
-    urcu_node<T>* m_old_node; /* Used in case of make and exchange as 2 different steps */
+    urcu_node< T >* m_rcu_node;
+    urcu_node< T >* m_old_node; /* Used in case of make and exchange as 2 different steps */
 };
 
-template <typename T>
+template < typename T >
 class urcu_data_batch {
 public:
     static urcu_data_batch< T >& instance() {
@@ -130,9 +132,9 @@ public:
         m_batch.insert(data);
     }
 
-    template <typename... Args>
+    template < typename... Args >
     void exchange(Args&&... args) {
-        std::vector< urcu_node<T>* > old_nodes;
+        std::vector< urcu_node< T >* > old_nodes;
         old_nodes.reserve(m_batch.size());
 
         {
@@ -145,37 +147,29 @@ public:
 
             synchronize_rcu();
 
-            for (auto &on : old_nodes) {
+            for (auto& on : old_nodes) {
                 call_rcu(&on->head, urcu_node< T >::free);
             }
         }
     }
 
 private:
-    std::mutex m_mutex;
-    std::set< urcu_data< T > *> m_batch;
+    std::mutex                  m_mutex;
+    std::set< urcu_data< T >* > m_batch;
 };
 
 class urcu_ctl {
 public:
     static thread_local bool _rcu_registered_already;
 
-    static void register_rcu() {
-        rcu_register_thread();
-    }
+    static void register_rcu() { rcu_register_thread(); }
 
-    static void declare_quiscent_state() {
-        rcu_quiescent_state();
-    }
+    static void declare_quiscent_state() { rcu_quiescent_state(); }
 
-    static void unregister_rcu() {
-        rcu_unregister_thread();
-    }
+    static void unregister_rcu() { rcu_unregister_thread(); }
 
-    static void sync_rcu() {
-        synchronize_rcu();
-    }
+    static void sync_rcu() { synchronize_rcu(); }
 };
 
 #define RCU_REGISTER_INIT thread_local bool sisl::urcu_ctl::_rcu_registered_already = false
-}
+} // namespace sisl
--- src/wisr/wisr_framework.hpp
+++ src/wisr/wisr_framework.hpp
@@ -10,59 +10,51 @@
 
 namespace sisl {
 
-template <typename DS, typename... Args>
+template < typename DS, typename... Args >
 class WrapperBuf {
 public:
-    template <class... Args1>
-    WrapperBuf(Args1&&... args) :
-            m_safe_buf(std::forward<Args1>(args)...),
-            m_args(std::forward<Args1>(args)...) {
-    }
+    template < class... Args1 >
+    WrapperBuf(Args1&&... args) : m_safe_buf(std::forward< Args1 >(args)...), m_args(std::forward< Args1 >(args)...) {}
 
-    ~WrapperBuf() {
-    }
+    ~WrapperBuf() {}
 
-    sisl::urcu_ptr< DS > get_safe() { return m_safe_buf.get(); }
+    sisl::urcu_ptr< DS >  get_safe() { return m_safe_buf.get(); }
     std::shared_ptr< DS > make_and_exchange() {
-        //auto n = m_safe_buf.make_and_exchange(std::forward<Args>(m_args)...);
-        //auto n = std::apply(m_safe_buf.make_and_exchange, m_args);
-        return _make_and_exchange(m_args, std::index_sequence_for<Args...>());
+        // auto n = m_safe_buf.make_and_exchange(std::forward<Args>(m_args)...);
+        // auto n = std::apply(m_safe_buf.make_and_exchange, m_args);
+        return _make_and_exchange(m_args, std::index_sequence_for< Args... >());
     }
 
     std::unique_ptr< DS > create_buf() {
-        //return std::make_unique< DS >(std::forward<Args>(m_args)...);
-        //return std::apply(std::make_unique< DS >, m_args);
-        return _create_buf(m_args, std::index_sequence_for<Args...>());
+        // return std::make_unique< DS >(std::forward<Args>(m_args)...);
+        // return std::apply(std::make_unique< DS >, m_args);
+        return _create_buf(m_args, std::index_sequence_for< Args... >());
     }
 
-    void make() {
-        return _make(m_args, std::index_sequence_for<Args...>());
+    void make() { return _make(m_args, std::index_sequence_for< Args... >()); }
+
+    std::shared_ptr< DS > exchange() { return m_safe_buf.exchange(); }
+
+private:
+    template < std::size_t... Is >
+    std::unique_ptr< DS > _create_buf(const std::tuple< Args... >& tuple, std::index_sequence< Is... >) {
+        return std::make_unique< DS >(std::get< Is >(tuple)...);
     }
 
-    std::shared_ptr< DS > exchange() {
-        return m_safe_buf.exchange();
+    template < std::size_t... Is >
+    std::shared_ptr< DS > _make_and_exchange(const std::tuple< Args... >& tuple, std::index_sequence< Is... >) {
+        return m_safe_buf.make_and_exchange(std::get< Is >(tuple)...);
+    }
+
+    template < std::size_t... Is >
+    void _make(const std::tuple< Args... >& tuple, std::index_sequence< Is... >) {
+        return m_safe_buf.make(std::get< Is >(tuple)...);
     }
 
 private:
-    template<std::size_t... Is>
-    std::unique_ptr< DS > _create_buf(const std::tuple<Args...>& tuple, std::index_sequence<Is...>) {
-        return std::make_unique<DS>(std::get<Is>(tuple)...);
-    }
-
-    template<std::size_t... Is>
-    std::shared_ptr< DS > _make_and_exchange(const std::tuple<Args...>& tuple, std::index_sequence<Is...>) {
-        return m_safe_buf.make_and_exchange(std::get<Is>(tuple)...);
-    }
-
-    template<std::size_t... Is>
-    void _make(const std::tuple<Args...>& tuple, std::index_sequence<Is...>) {
-        return m_safe_buf.make(std::get<Is>(tuple)...);
-    }
-
-private:
-    sisl::urcu_data< DS >  m_safe_buf;
-    std::tuple< Args... >  m_args;
-    bool m_is_batch_on;
+    sisl::urcu_data< DS > m_safe_buf;
+    std::tuple< Args... > m_args;
+    bool                  m_is_batch_on;
 };
 
 /* This class implements a generic wait free writer framework to build a wait free writer structures on top of it.
@@ -70,32 +62,29 @@ private:
  * using rcu and per thread buffer. Thus it can be typically used on structures which are very frequently updated but
  * rarely read (say metrics collection, list of garbage entries to cleanup etc)..
  */
-template <typename DS, typename... Args>
+template < typename DS, typename... Args >
 class wisr_framework {
 public:
-    template <class... Args1>
-    wisr_framework(Args1&&... args) :
-            m_buffer(std::forward<Args1>(args)...) {
+    template < class... Args1 >
+    wisr_framework(Args1&&... args) : m_buffer(std::forward< Args1 >(args)...) {
         m_base_obj = m_buffer->create_buf();
     }
 
-    DS* insertable() {
-        return m_buffer->get_safe().get();
-    }
+    DS* insertable() { return m_buffer->get_safe().get(); }
 
     DS* now() {
-        std::lock_guard<std::mutex> lg(m_rotate_mutex);
+        std::lock_guard< std::mutex > lg(m_rotate_mutex);
         _rotate();
         return m_base_obj.get();
     }
 
     DS* delayed() {
-        std::lock_guard<std::mutex> lg(m_rotate_mutex);
+        std::lock_guard< std::mutex > lg(m_rotate_mutex);
         return m_base_obj.get();
     }
 
     std::unique_ptr< DS > get_copy_and_reset() {
-        std::lock_guard<std::mutex> lg(m_rotate_mutex);
+        std::lock_guard< std::mutex > lg(m_rotate_mutex);
         _rotate();
 
         auto ret = std::move(m_base_obj);
@@ -111,8 +100,8 @@ public:
      * 3. Call deferred()
      */
     void prepare_rotate() {
-        std::lock_guard<std::mutex> lg(m_rotate_mutex);
-        m_buffer.access_all_threads([](WrapperBuf<DS, Args...> *ptr, bool is_thread_running) {
+        std::lock_guard< std::mutex > lg(m_rotate_mutex);
+        m_buffer.access_all_threads([](WrapperBuf< DS, Args... >* ptr, bool is_thread_running) {
             (void)is_thread_running;
             ptr->make();
             return false;
@@ -120,12 +109,14 @@ public:
     }
 
     DS* deferred() {
-        std::lock_guard<std::mutex> lg(m_rotate_mutex);
-        auto base_raw = m_base_obj.get();
-        m_buffer.access_all_threads([base_raw](WrapperBuf<DS, Args...> *ptr, bool is_thread_running) {
+        std::lock_guard< std::mutex > lg(m_rotate_mutex);
+        auto                          base_raw = m_base_obj.get();
+        m_buffer.access_all_threads([base_raw](WrapperBuf< DS, Args... >* ptr, bool is_thread_running) {
             (void)is_thread_running;
             auto old_ptr = ptr->exchange();
-            DS::merge(base_raw, old_ptr.get());
+            if (old_ptr) {
+                DS::merge(base_raw, old_ptr.get());
+            }
             return true;
         });
         return m_base_obj.get();
@@ -135,20 +126,20 @@ private:
     // This method assumes that rotate mutex is already held
     void _rotate() {
         auto base_raw = m_base_obj.get();
-        m_buffer.access_all_threads([base_raw](WrapperBuf<DS, Args...> *ptr, bool is_thread_running) {
+        m_buffer.access_all_threads([base_raw](WrapperBuf< DS, Args... >* ptr, bool is_thread_running) {
             (void)is_thread_running;
             auto old_ptr = ptr->make_and_exchange();
             DS::merge(base_raw, old_ptr.get());
             return true;
         });
     }
+
 private:
     sisl::ExitSafeThreadBuffer< WrapperBuf< DS, Args... >, Args... > m_buffer;
-    std::mutex m_rotate_mutex;
-    std::unique_ptr< DS > m_base_obj;
+    std::mutex                                                       m_rotate_mutex;
+    std::unique_ptr< DS >                                            m_base_obj;
 };
 
 } // namespace sisl
 
-
-#endif //SISL_FDS_WAITFREE_WRITE_DS_HPP
+#endif // SISL_FDS_WAITFREE_WRITE_DS_HPP
