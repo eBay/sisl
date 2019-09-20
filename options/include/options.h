0834cc8027aa49ac2ec884d2fb83994c1e516970
Brian Szmyd <bszmyd@ebay.com>
2019-09-20 13:01:51 -0700

    v0.1.6 - Fix leak on shutdown.

--- options/include/options.h
+++ options/include/options.h
@@ -4,8 +4,6 @@
 
 #pragma once
 
-#include <iostream>
-#include <memory>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/stringize.hpp>
@@ -17,10 +15,13 @@
 #include <boost/preprocessor/variadic/to_seq.hpp>
 #include <boost/preprocessor/variadic/to_tuple.hpp>
 #include <cxxopts/cxxopts.hpp>
+#include <iostream>
+#include <memory>
 
 namespace sds_options {
-template<bool...> class bool_pack;
-template<bool...b>
+template <bool...>
+class bool_pack;
+template <bool... b>
 using all_true = std::is_same<bool_pack<true, b...>, bool_pack<b..., true>>;
 
 template <typename T>
@@ -32,63 +33,81 @@ using shared_opt_res = shared<cxxopts::ParseResult>;
 extern shared_opt GetOptions() __attribute__((weak));
 extern shared_opt_res GetResults() __attribute__((weak));
 struct SdsOption {
-   template<class... Args>
-   explicit SdsOption(std::string const& group, Args... args)
-   {
-      auto o = GetOptions();
-      o->add_option(group, args...);
-   }
+  template <class... Args>
+  explicit SdsOption(std::string const& group, Args... args) {
+    auto o = GetOptions();
+    o->add_option(group, args...);
+  }
 };
-}
+}  // namespace sds_options
 
-#define SDS_OPTION(r, group, args) \
-   sds_options::SdsOption const BOOST_PP_CAT(_option_, BOOST_PP_TUPLE_ELEM(0, args)) \
-      {BOOST_PP_STRINGIZE(group), BOOST_PP_TUPLE_REM_CTOR(BOOST_PP_TUPLE_REMOVE(args, 0))};
+#define SDS_OPTION(r, group, args)                                         \
+  sds_options::SdsOption const BOOST_PP_CAT(_option_,                      \
+                                            BOOST_PP_TUPLE_ELEM(0, args)){ \
+      BOOST_PP_STRINGIZE(group),                                           \
+      BOOST_PP_TUPLE_REM_CTOR(BOOST_PP_TUPLE_REMOVE(args, 0))};
 
-#define SDS_OPTION_GROUP(group, ...) \
-namespace sds_options { \
-   struct BOOST_PP_CAT(options_module_, group) { \
-      BOOST_PP_SEQ_FOR_EACH(SDS_OPTION, (group), BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)); \
-   }; \
-   extern BOOST_PP_CAT(options_module_, group)* BOOST_PP_CAT(load_options_, group)() { return new BOOST_PP_CAT(options_module_, group)(); } \
-}
+#define SDS_OPTION_GROUP(group, ...)                                     \
+  namespace sds_options {                                                \
+  struct BOOST_PP_CAT(options_module_, group) {                          \
+    BOOST_PP_SEQ_FOR_EACH(SDS_OPTION, (group),                           \
+                          BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__));        \
+  };                                                                     \
+  extern BOOST_PP_CAT(options_module_, group) *                          \
+      BOOST_PP_CAT(load_options_, group)() {                             \
+    return new BOOST_PP_CAT(options_module_, group)();                   \
+  }                                                                      \
+  extern void BOOST_PP_CAT(unload_options_,                              \
+                           group)(BOOST_PP_CAT(options_module_, group) * \
+                                  ptr) {                                 \
+    delete ptr;                                                          \
+  }                                                                      \
+  }
 
-#define SDS_OPTION_ENABLE(r, _, group) \
-   namespace sds_options { \
-      struct BOOST_PP_CAT(options_module_, group); \
-      BOOST_PP_CAT(options_module_, group)* BOOST_PP_CAT(load_options_, group)(); \
-   } \
-   static BOOST_PP_CAT(sds_options::options_module_, group) const * BOOST_PP_CAT(options_group_, group);
+#define SDS_OPTION_ENABLE(r, _, group)                                         \
+  namespace sds_options {                                                      \
+  struct BOOST_PP_CAT(options_module_, group);                                 \
+  BOOST_PP_CAT(options_module_, group) * BOOST_PP_CAT(load_options_, group)(); \
+  void BOOST_PP_CAT(unload_options_,                                           \
+                    group)(BOOST_PP_CAT(options_module_, group) *);            \
+  }                                                                            \
+  static std::unique_ptr<BOOST_PP_CAT(sds_options::options_module_, group),    \
+                         decltype(&BOOST_PP_CAT(sds_options::unload_options_,  \
+                                                group))>                       \
+      BOOST_PP_CAT(options_group_, group)(                                     \
+          nullptr, &BOOST_PP_CAT(sds_options::unload_options_, group));
 
-#define SDS_OPTIONS_ENABLE(...)                 \
-   BOOST_PP_SEQ_FOR_EACH(SDS_OPTION_ENABLE, _, BOOST_PP_TUPLE_TO_SEQ(BOOST_PP_TUPLE_PUSH_FRONT(BOOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__), main))) \
-   static sds_options::shared_opt options_;     \
-   static sds_options::shared_opt_res results_; \
-      namespace sds_options {                   \
-      shared_opt GetOptions() {                 \
-          return options_;                      \
-      }                                         \
-      shared_opt_res GetResults() {             \
-          return results_;                      \
-      }                                         \
-                                                \
-      void SetOptions(shared_opt&& options) {   \
-          options_ = std::move(options);        \
-      }                                         \
-   }
+#define SDS_OPTIONS_ENABLE(...)                                              \
+  BOOST_PP_SEQ_FOR_EACH(SDS_OPTION_ENABLE, _,                                \
+                        BOOST_PP_TUPLE_TO_SEQ(BOOST_PP_TUPLE_PUSH_FRONT(     \
+                            BOOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__), main))) \
+  static sds_options::shared_opt options_;                                   \
+  static sds_options::shared_opt_res results_;                               \
+  namespace sds_options {                                                    \
+  shared_opt GetOptions() { return options_; }                               \
+  shared_opt_res GetResults() { return results_; }                           \
+                                                                             \
+  void SetOptions(shared_opt&& options) { options_ = std::move(options); }   \
+  }
 
 #define SDS_OPTIONS (*sds_options::GetResults())
 #define SDS_PARSER (*sds_options::GetOptions())
 
-#define SDS_OPTION_LOAD(r, _, group) BOOST_PP_CAT(options_group_, group) = BOOST_PP_CAT(sds_options::load_options_, group)();
+#define SDS_OPTION_LOAD(r, _, group)                         \
+  BOOST_PP_CAT(options_group_, group) =                      \
+      decltype(BOOST_PP_CAT(options_group_, group))(         \
+          BOOST_PP_CAT(sds_options::load_options_, group)(), \
+          &BOOST_PP_CAT(sds_options::unload_options_, group));
 
-#define SDS_OPTIONS_LOAD(argc, argv, ...) \
-   sds_options::SetOptions(std::make_shared<cxxopts::Options>(argv[0])); \
-   BOOST_PP_SEQ_FOR_EACH(SDS_OPTION_LOAD, _, BOOST_PP_TUPLE_TO_SEQ(BOOST_PP_TUPLE_PUSH_FRONT(BOOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__), main))) \
-   results_ = std::make_shared<cxxopts::ParseResult>(SDS_PARSER.parse(argc, argv)); \
-   if (SDS_OPTIONS.count("help")) { \
-      std::cout << SDS_PARSER.help({}) << std::endl; \
-      exit(0); \
-   }
-
+#define SDS_OPTIONS_LOAD(argc, argv, ...)                                    \
+  sds_options::SetOptions(std::make_shared<cxxopts::Options>(argv[0]));      \
+  BOOST_PP_SEQ_FOR_EACH(SDS_OPTION_LOAD, _,                                  \
+                        BOOST_PP_TUPLE_TO_SEQ(BOOST_PP_TUPLE_PUSH_FRONT(     \
+                            BOOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__), main))) \
+  results_ =                                                                 \
+      std::make_shared<cxxopts::ParseResult>(SDS_PARSER.parse(argc, argv));  \
+  if (SDS_OPTIONS.count("help")) {                                           \
+    std::cout << SDS_PARSER.help({}) << std::endl;                           \
+    exit(0);                                                                 \
+  }
 
