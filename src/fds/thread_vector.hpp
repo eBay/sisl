be8511446b63d14d4abf6a59f110a9219ec5c438
rishabh mittal <rimittal@ebay.com>
2020-06-16 15:59:54 -0700

    incorporate review comments

--- src/fds/thread_vector.hpp
+++ src/fds/thread_vector.hpp
@@ -2,7 +2,6 @@
 
 #include "utility/thread_buffer.hpp"
 namespace sisl {
-typedef std::pair< int, int > thread_vector_iterator;
 
 /* This data structure inserts elements into per thread buffer and provide apis to access the elements.
  * Note :- 1. It assumes that insert and access won't go in parallel.
@@ -10,30 +9,38 @@ typedef std::pair< int, int > thread_vector_iterator;
  */
 template < typename T >
 class ThreadVector {
+
 public:
+    using thread_buffer_iterator = std::pair< uint32_t, std::vector< T >* >;
+    using thread_vector_iterator = std::pair< thread_buffer_iterator, int >;
+
     ThreadVector(uint64_t size) : thread_buffer(size) {}
     void push_back(T ele) {
         auto thread_vector = thread_buffer.get();
         thread_vector.push_back(ele);
     }
 
-    T begin(thread_vector_iterator& it) { return (0, 0, it); }
-    T next(thread_vector_iterator& it) { return (next(it.first, ++it.second, it)); }
-    bool end(thread_vector_iterator& it) {
-        int buf_indx = it.first;
-        int ele_indx = it.second;
-        if (buf_indx != thread_buffer.get_count() || ele_indx != thread_buffer[buf_indx].size()) { return false; }
-        return true;
+    T* begin(thread_vector_iterator& v_it) {
+        auto b_it = thread_buffer.begin_iterator();
+        if (b_it.second == nullptr) { return nullptr; }
+        v_it = thread_vector_iterator(b_it, -1);
+        return (next(v_it));
     }
 
-private:
-    T next(int buf_indx, int ele_indx, thread_vector_iterator& it) {
-        while (ele_indx == thread_buffer[buf_indx].size() && buf_indx < thread_buffer.get_count()) {
-            ++buf_indx;
-            ele_indx = 0;
+    T* next(thread_vector_iterator& v_it) {
+        auto b_it = v_it.first;
+        int ele_indx = v_it.second;
+        auto buf = b_it.second;
+        assert(buf != nullptr);
+
+        while (++ele_indx == buf.size()) {
+            b_it = thread_buffer.next_iterator(b_it);
+            if (b_it.second == nullptr) { return nullptr; }
+            buf = b_it.second;
+            ele_indx = -1;
         }
-        it = std::pair< int, int >(buf_indx, ele_indx);
-        return thread_buffer[buf_indx][ele_indx];
+        v_it = thread_vector_iterator(b_it, ele_indx);
+        return (&(buf[ele_indx]));
     }
 
 private:
