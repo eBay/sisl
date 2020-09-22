f27be75a84cbb816752c43fb14c857228937cd7a
Bryan Zimmerman <brzimmerman@ebay.com>
2020-09-21 20:13:38 -0400

    Finished refactoring bitword

--- src/fds/bitword.hpp
+++ src/fds/bitword.hpp
@@ -12,6 +12,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstdint>
+#include <iostream>
 #include <sstream>
 
 namespace sisl {
@@ -160,6 +161,26 @@ static constexpr uint8_t logBase2(const uint64_t v) {
     return MultiplyDeBruijnBitPosition[((v ^ (v-1)) * static_cast<uint64_t>(0x03F79D71B4CB0A89)) >> 58];
 }
 
+static constexpr uint8_t get_set_bit_count(const uint64_t v) {
+//#ifdef __x86_64
+    // return __builtin_popcountl(m_Bits.get());
+//#else
+    constexpr uint64_t m1{0x5555555555555555}; // binary: 0101...
+    constexpr uint64_t m2{0x3333333333333333}; // binary: 00110011..
+    constexpr uint64_t m4{0x0f0f0f0f0f0f0f0f}; // binary:  4 zeros,  4 ones ...
+    // constexpr uint64_t m8{0x00ff00ff00ff00ff};   // binary:  8 zeros,  8 ones ...
+    // constexpr uint64_t m16{0x0000ffff0000ffff};  // binary: 16 zeros, 16 ones ...
+    // constexpr uint64_t m32{0x00000000ffffffff};  // binary: 32 zeros, 32 ones
+    constexpr uint64_t h01{0x0101010101010101}; // the sum of 256 to the power of 0,1,2,3...
+
+    uint64_t x{v};
+    x -= (x >> 1) & m1;                             // put count of each 2 bits into those 2 bits
+    x = (x & m2) + ((x >> 2) & m2);                 // put count of each 4 bits into those 4 bits
+    x = (x + (x >> 4)) & m4;                        // put count of each 8 bits into those 8 bits
+    return static_cast< uint8_t >((x * h01) >> 56); // returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+//#endif
+}
+
 enum class bit_match_type : uint8_t { no_match, lsb_match, mid_match, msb_match };
 
 struct bit_filter {
@@ -183,15 +204,18 @@ struct bit_filter {
 template < typename Word >
 class Bitword {
     // class __attribute__((__packed__)) Bitword {
+
 public:
     static constexpr uint8_t bits() { return (sizeof(Word) * 8); }
     typedef typename Word::word_t word_t;
+    static_assert(std::is_unsigned_v< word_t > && sizeof(word_t) <= sizeof(uint64_t),
+                  "Underlying type must be unsigned of 64 bits or less.");
 
     Bitword() { m_Bits.set(0); }
     explicit Bitword(const Word& b) { m_Bits.set(b); }
     explicit Bitword(const word_t& val) { m_Bits.set(val); }
     Bitword(const Bitword&) = delete;
-    Bitword(Bitword&&) noexcept  = delete;
+    Bitword(Bitword&&) noexcept = delete;
     Bitword& operator=(const Bitword&) = delete;
     Bitword& operator=(Bitword&&) noexcept = delete;
 
@@ -201,34 +225,22 @@ public:
     // @brief:
     // Total number of bits set in the bitset
     //
-    uint8_t get_set_count() const {
-//#ifdef __x86_64
-        //return __builtin_popcountl(m_Bits.get());
-//#else
-    static constexpr uint64_t m1{0x5555555555555555};   // binary: 0101...
-    static constexpr uint64_t m2{0x3333333333333333};   // binary: 00110011..
-    static constexpr uint64_t m4{0x0f0f0f0f0f0f0f0f};   // binary:  4 zeros,  4 ones ...
-    //static constexpr uint64_t m8{0x00ff00ff00ff00ff};   // binary:  8 zeros,  8 ones ...
-    //static constexpr uint64_t m16{0x0000ffff0000ffff};  // binary: 16 zeros, 16 ones ...
-    //static constexpr uint64_t m32{0x00000000ffffffff};  // binary: 32 zeros, 32 ones
-    static constexpr uint64_t h01{0x0101010101010101};  // the sum of 256 to the power of 0,1,2,3...
-
-    uint64_t x{m_Bits.get()};
-    x -= (x >> 1) & m1;    ;         // put count of each 2 bits into those 2 bits
-    x = (x & m2) + ((x >> 2) & m2); // put count of each 4 bits into those 4 bits
-    x = (x + (x >> 4)) & m4;        // put count of each 8 bits into those 8 bits
-    return static_cast<uint8_t>((x * h01) >> 56);  // returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
-//#endif
+    uint8_t get_set_count() const { return get_set_bit_count(m_Bits.get());
     }
-
     //
     // @brief:
     // Total number of bits reset in the bitset
     //
-    uint8_t get_reset_count() const { return bits() - get_set_count(); }
+    uint8_t get_reset_count() const { return bits() - get_set_bit_count(m_Bits.get());
+    }
 
-    word_t set_bits(const uint8_t start, const uint8_t nbits) { return set_reset_bits(start, nbits, true /* set */); }
+    word_t set_bits(const uint8_t start, const uint8_t nbits)
+    {
+        assert(start < bits());
+        return set_reset_bits(start, nbits, true /* set */);
+    }
     word_t reset_bits(const uint8_t start, const uint8_t nbits) {
+        assert(start < bits());
         return set_reset_bits(start, nbits, false /* set */);
     }
 
@@ -240,6 +252,7 @@ public:
     // Returns the final bitmap set of entry_type
     //
     word_t set_reset_bit(const uint8_t start, const bool set) {
+        assert(start < bits());
         if (set) {
             return m_Bits.or_with(bit_mask[start]);
         } else {
@@ -256,6 +269,7 @@ public:
     // Returns the final bitmap set of entry_type
     //
     word_t set_reset_bits(const uint8_t start, const uint8_t nbits, const bool set) {
+        assert(start < bits());
         if (nbits == 1) { return set_reset_bit(start, set); }
 
         const uint8_t wanted_bits{std::min<uint8_t>(bits() - start, nbits)};
@@ -267,66 +281,70 @@ public:
         }
     }
 
-    bool get_bitval(const uint8_t bit) const { return m_Bits.get() & bit_mask[bit]; }
+    bool get_bitval(const uint8_t bit) const { return (m_Bits.get() & bit_mask[bit]); }
 
     // @brief
     // is_bit_set_reset: Is bits either set or reset from start bit
     //
     bool is_bit_set_reset(const uint8_t start, const bool check_for_set) const {
+        assert(start < bits());
         const uint64_t v{m_Bits.get() & bit_mask[start]};
-        return check_for_set ? (v != 0) : (v == 0);
+        return check_for_set ? (v != static_cast< uint64_t >(0)) : (v == static_cast< uint64_t >(0));
     }
 
     bool is_bits_set_reset(const uint8_t start, const uint8_t nbits, const bool check_for_set) const {
+        assert(start < bits());
         if (nbits == 1) { return (is_bit_set_reset(start, check_for_set)); }
 
         const word_t actual{extract(start, nbits)};
         const uint64_t expected{check_for_set ? consecutive_bitmask[nbits - 1] : 0};
-        return (actual == expected);
+        return (static_cast<uint64_t>(actual) == expected);
     }
 
     bool get_next_set_bit(const uint8_t start, uint8_t* const p_set_bit) const {
+        assert(start < bits());
+        assert(p_set_bit);
         const word_t e{extract(start, bits())};
-        if (e) { *p_set_bit = get_trailing_zeros(e) + start; }
+        if (e) {
+            *p_set_bit = get_trailing_zeros(e) + start;
+            return true;
+        } else {
+            return false;
+        }
         //if (e) { *p_set_bit = ffsll(e) - 1 + start; }
-        //*p_set_bit = (e != 0) && logBase2(e & ~e);
-        return (e != 0);
     }
 
     bool get_next_reset_bit(const uint8_t start, uint8_t* const p_reset_bit) const {
-        const word_t e{extract(start, bits())};
-
-        // Isolated the rightmost 0th bit
-        const word_t x{~e & (e + 1)};
-        if (x == 0) { return false; }
-        *p_reset_bit = logBase2(x) + start;
-        return (*p_reset_bit < bits());
+        assert(start < bits());
+        assert(p_reset_bit);
+        const word_t e{~extract(start, bits())};
+        if (e == static_cast< word_t >(0))
+        {
+            return false;
+        } else {
+            *p_reset_bit = get_trailing_zeros(e) + start;
+            return (*p_reset_bit < bits());
+        }
     }
 
     uint8_t get_next_reset_bits(const uint8_t start, uint8_t* const pcount) const {
+        assert(start < bits());
+        assert(pcount);
         *pcount = 0;
         uint8_t first_0bit{0};
         const word_t e{extract(start, bits())};
-
         if (e == 0) {
             // Shortcut for all zeros
+            first_0bit = start;
             *pcount = bits() - start;
         } else {
             // Find the first 0th bit in the word
-            word_t x{(~e) & (e + 1)};
-            if (x == 0) {
-                // There are no reset bits in the word beyond start
+            first_0bit = static_cast<uint8_t>(get_trailing_zeros(~e) + start);
+            if (first_0bit >= bits()) {
+                // No more zero's here in our range.
+                first_0bit = bits();
             } else {
-                first_0bit = logBase2(x);
-                e >>= first_0bit;
-
-                // Find the first 1st bit in the word
-                x = e & (-e);
-                if (x == 0) {
-                    *pcount = (bits() - start - first_0bit);
-                } else {
-                    *pcount = logBase2(x); // next bit number with 1 should be the count of 0s
-                }
+                *pcount = bits() - start - get_set_bit_count(e);
             }
         }
         return first_0bit;
@@ -337,7 +355,8 @@ public:
         uint8_t start_bit;
         uint8_t count;
 
-        bit_match_result(const bit_match_type match_type, const uint8_t start_bit, const uint8_t count = 0) :
+        bit_match_result(const bit_match_type match_type = bit_match_type::no_match, const uint8_t start_bit = 0,
+                         const uint8_t count = 0) :
                 match_type{match_type}, start_bit{start_bit}, count{count} {}
         bit_match_result(const bit_match_result&) = default;
         bit_match_result(bit_match_result&&) noexcept = default;
@@ -345,14 +364,16 @@ public:
         bit_match_result& operator=(bit_match_result&&) noexcept = delete;
 
         std::string to_string() const {
-            std::ostringstream ss;
+            std::ostringstream ss{};
             ss << "match_type=" << static_cast< uint16_t >(match_type)
                << " start_bit=" << static_cast< uint16_t >(start_bit) << " count=" << static_cast< uint16_t >(count);
             return ss.str();
         }
     };
 
+    // match the number of bits required at the beginning(lsb), middle(mid), end(msb) of the value
     bit_match_result get_next_reset_bits_filtered(const uint8_t offset, const bit_filter& filter) const {
+        assert(offset < bits());
         bit_match_result result{bit_match_type::no_match, offset};
         bool lsb_search{offset == 0};
 
@@ -362,17 +383,20 @@ public:
             result.count = 0;
             uint8_t first_0bit{get_trailing_zeros(~e)};
             result.start_bit += first_0bit;
-            if (first_0bit >= nbits) {
+            if (first_0bit > nbits) {
                 // No more zero's here in our range.
                 break;
             }
 
-            e = e >> first_0bit;
-            nbits -= first_0bit;
-            result.count = e ? get_trailing_zeros(e) : nbits;
+            if (first_0bit > 0) {
+                // remove all the 1's group
+                e = e >> first_0bit;
+                nbits -= first_0bit;
+            }
+            result.count = (e > static_cast< word_t >(0)) ? get_trailing_zeros(e) : nbits;
 
             if (lsb_search) {
-                if ((first_0bit == 0) && (result.count >= filter.n_lsb_reqd)) {
+                if ((first_0bit == static_cast<uint8_t>(0)) && (result.count >= filter.n_lsb_reqd)) {
                     // We matched lsb with required count
                     result.match_type = bit_match_type::lsb_match;
                     break;
@@ -385,82 +409,101 @@ public:
                 break;
             }
 
-            // Not enought count for lsb and mid match, keep going
+            // Not enough count for lsb and mid match, keep going by removing 0's group
             e = e >> result.count;
-            if (e == 0) { break; }
-
+            if (e == static_cast< word_t >(0))
+            {
+                // this group of 0's was at the end(msb)
+                if (result.count >= filter.n_msb_reqd) { result.match_type = bit_match_type::msb_match; }
+                break;
+            }
+             
             nbits -= result.count;
             result.start_bit += result.count;
         }
 
-        if ((result.match_type == bit_match_type::no_match) && (result.count >= filter.n_msb_reqd)) {
-            result.match_type = bit_match_type::msb_match;
-        }
         return result;
     }
 
-    bool set_next_reset_bit(int start, int maxbits, int* p_bit) {
-        bool found = get_next_reset_bit(start, p_bit);
+    bool set_next_reset_bit(const uint8_t start, const uint8_t maxbits, uint8_t* const p_bit) {
+        assert(start < bits());
+        assert(p_bit);
+        const bool found{get_next_reset_bit(start, p_bit)};
         if (!found || (*p_bit >= maxbits)) { return false; }
 
         set_reset_bit(*p_bit, true);
         return true;
     }
 
-    bool set_next_reset_bit(int start, int* p_bit) { return set_next_reset_bit(start, bits(), p_bit); }
+    bool set_next_reset_bit(const uint8_t start, uint8_t* const* p_bit) {
+        return set_next_reset_bit(start, bits(), p_bit);
+    }
 
-    word_t right_shift(int nbits) { return m_Bits.right_shift(); }
+    word_t right_shift(const uint8_t nbits) { return m_Bits.right_shift(nbits); }
 
-    int get_max_contigous_reset_bits(int start, uint32_t* pmax_count) const {
-        uint64_t cur_bit = 0;
-        uint64_t prev_bit = 0;
-        int ret_bit = -1;
-        uint64_t count;
+    uint8_t get_max_contiguous_reset_bits(const uint8_t start, uint8_t* const pmax_count) const {
+        assert(start < bits());
+        assert(pmax_count);
+
         *pmax_count = 0;
-
-        if (start == bits()) { return -1; }
-
-        word_t e = extract(start, bits());
-        while ((e != 0)) {
-            uint64_t x = e & (-e);
-            e &= ~x;
-            cur_bit = logBase2(x);
-            count = cur_bit - prev_bit;
-
-            if (count > *pmax_count) {
-                ret_bit = (int)prev_bit + start;
-                *pmax_count = (uint32_t)count;
+        uint8_t offset{start};
+        word_t e{extract(start, bits())};
+        uint8_t start_largest_group{std::numeric_limits<uint8_t>::max()};
+        while (offset < bits()) {
+            if (e == 0) {
+                // Shortcut for all zeros
+                const uint8_t num_reset_bits{static_cast<uint8_t>(bits() - offset)};
+                if (num_reset_bits > *pmax_count)
+                {
+                    *pmax_count = num_reset_bits;
+                    start_largest_group = offset;
+                }
+                break;
+            } else {
+                // Find the first 0th bit in the word
+                const uint8_t first_0bit{static_cast< uint8_t >(get_trailing_zeros(~e))};
+                if (first_0bit >= bits()) {
+                    // No more zero's here in our range.
+                    break;
+                } else {
+                    if (first_0bit > 0) {
+                        // remove all the 1's group
+                        e = e >> first_0bit;
+                        offset += first_0bit;
+                    }
+                    const uint8_t num_reset_bits {static_cast<uint8_t>((e > static_cast< word_t >(0)) ? 
+                                                                       get_trailing_zeros(e) :
+                                                                       bits() - offset)};
+                    if (num_reset_bits > *pmax_count) {
+                        *pmax_count = num_reset_bits;
+                        start_largest_group = offset;
+                    }
+                    // remove the all 0's group
+                    offset += num_reset_bits;
+                    e >>= num_reset_bits;
+                }
             }
-            prev_bit = cur_bit + 1;
         }
 
-        // Find the leading 0s
-        count = bits() - start - prev_bit;
-        if (count > *pmax_count) {
-            ret_bit = (int)prev_bit + start;
-            *pmax_count = (uint32_t)count;
-        }
-
-        return ret_bit;
+        return start_largest_group;
     }
 
-    word_t to_integer() { return m_Bits.get(); }
+    word_t to_integer() const { return m_Bits.get(); }
 
-    std::string to_string() {
-        char str[bits() + 1] = {0};
-        word_t e = m_Bits.get();
+    std::string to_string() const {
+        std::ostringstream oSS{};
+        word_t e{m_Bits.get()};
+        word_t mask{static_cast<word_t>(bit_mask[bits() - 1])};
+        for (uint8_t bit{0}; bit < bits(); ++bit, mask >>= 1)
+        {
+            oSS << (((e & mask) == mask) ? '1' : '0');
 
-        str[bits()] = '\0';
-        for (int i = bits() - 1; i >= 0; i--) {
-            str[i] = (e & 1) ? '1' : '0';
-            e >>= 1;
         }
-        return std::string(str);
+        return oSS.str();
     }
 
-    void print() {
-        std::string str = to_string();
-        printf("%s\n", str.c_str());
+    void print() const {
+        std::cout << to_string() << std::endl;
     }
 
 private:
@@ -550,7 +593,7 @@ public:
         while (!m_Value.compare_exchange_weak(old_value, new_value, std::memory_order_acq_rel)) {
             old_value = m_Value.get(std::memory_order_acquire);
             new_value = old_value >> nbits;
-        };
+        }
 
         return new_value;
     }
--- src/fds/tests/test_bitword.cpp
+++ src/fds/tests/test_bitword.cpp
@@ -1,6 +1,7 @@
 
 #include <cstdint>
 #include <iostream>
+#include <limits>
 
 #include <gtest/gtest.h>
 #include <sds_logging/logging.h>
@@ -19,18 +20,18 @@ static bool validate(const uint64_t val, const uint8_t offset, bit_filter filter
 
     const auto result{bword.get_next_reset_bits_filtered(offset, filter)};
     if (result.match_type != exp_match) {
-        LOGINFO("Val={} offset={} filter[{}] Expected type={} but got {}, result[{}]: FAILED", val, static_cast<uint16_t>(offset),
-                filter.to_string(), static_cast<uint16_t>(exp_match), static_cast<uint16_t>(result.match_type), result.to_string());
+        //LOGINFO("Val={} offset={} filter[{}] Expected type={} but got {}, result[{}]: FAILED", val, static_cast<uint16_t>(offset),
+        //        filter.to_string(), static_cast<uint16_t>(exp_match), static_cast<uint16_t>(result.match_type), result.to_string());
         return false;
     }
 
     if ((result.match_type != bit_match_type::no_match) && (result.start_bit != exp_start)) {
-        LOGINFO("Val={} offset={} filter[{}] Expected start bit={} but got {}, result[{}] : FAILED", val, static_cast<uint16_t>(offset),
-                filter.to_string(), static_cast<uint16_t>(exp_start), static_cast<uint16_t>(result.start_bit), result.to_string());
+        //LOGINFO("Val={} offset={} filter[{}] Expected start bit={} but got {}, result[{}] : FAILED", val, static_cast<uint16_t>(offset),
+        //        filter.to_string(), static_cast<uint16_t>(exp_start), static_cast<uint16_t>(result.start_bit), result.to_string());
         return false;
     }
 
-    LOGINFO("Val={} offset={} filter[{}] result[{}] : Passed", val, static_cast<uint16_t>(offset), filter.to_string(), result.to_string());
+    //LOGINFO("Val={} offset={} filter[{}] result[{}] : Passed", val, static_cast<uint16_t>(offset), filter.to_string(), result.to_string());
     return true;
 }
 
@@ -69,6 +70,19 @@ TEST_F(BitwordTest, TestSetCount) {
     ASSERT_EQ(word4.get_set_count(), 64);
 }
 
+TEST_F(BitwordTest, TestResetCount) {
+    const Bitword< unsafe_bits< uint64_t > > word1{0x1};
+    ASSERT_EQ(word1.get_reset_count(), 63);
+
+    const Bitword< unsafe_bits< uint64_t > > word2{0x0};
+    ASSERT_EQ(word2.get_reset_count(), 64);
+
+    const Bitword< unsafe_bits< uint64_t > > word3{0x100000000};
+    ASSERT_EQ(word3.get_reset_count(), 63);
+
+    const Bitword< unsafe_bits< uint64_t > > word4{0xFFFFFFFFFFFFFFFF};
+    ASSERT_EQ(word4.get_reset_count(), 0);
+}
 
 TEST_F(BitwordTest, SetResetBit) {
     Bitword< unsafe_bits< uint64_t > > word1{0x0};
@@ -92,7 +106,125 @@ TEST_F(BitwordTest, ResetBits) {
     ASSERT_EQ(word1.reset_bits(62, 2), static_cast< uint64_t >(0x3FFFFFFFFFFFFFFC));
 }
 
-TEST_F(BitwordTest, TestGetNextResetBitsFiltered)
+TEST_F(BitwordTest, GetBitVal) {
+    const Bitword< unsafe_bits< uint64_t > > word1{0x8000000000000001};
+    ASSERT_TRUE(word1.get_bitval(63));
+    ASSERT_TRUE(word1.get_bitval(0));
+    ASSERT_FALSE(word1.get_bitval(62));
+    ASSERT_FALSE(word1.get_bitval(1));
+}
+
+TEST_F(BitwordTest, IsBitSetReset) {
+    const Bitword< unsafe_bits< uint64_t > > word1{0x8000000000000001};
+    ASSERT_TRUE(word1.is_bit_set_reset(63, true));
+    ASSERT_TRUE(word1.is_bit_set_reset(0, true));
+    ASSERT_TRUE(word1.is_bit_set_reset(62, false));
+    ASSERT_TRUE(word1.is_bit_set_reset(1, false));
+}
+
+TEST_F(BitwordTest, IsBitsSetReset) {
+    const Bitword< unsafe_bits< uint64_t > > word1{0xC000000000000003};
+    ASSERT_TRUE(word1.is_bits_set_reset(62, 2, true));
+    ASSERT_TRUE(word1.is_bits_set_reset(0, 2, true));
+    ASSERT_TRUE(word1.is_bits_set_reset(60, 2, false));
+    ASSERT_TRUE(word1.is_bits_set_reset(2, 2, false));
+}
+
+TEST_F(BitwordTest, GetNextSetBit) {
+    uint8_t bit_pos;
+    const Bitword< unsafe_bits< uint64_t > > word1{0x05};
+    ASSERT_TRUE(word1.get_next_set_bit(0, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast<uint8_t>(0));
+    ASSERT_TRUE(word1.get_next_set_bit(1, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(2));
+
+    const Bitword< unsafe_bits< uint64_t > > word2{0x8000000000000000};
+    ASSERT_TRUE(word2.get_next_set_bit(0, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(63));
+    ASSERT_TRUE(word2.get_next_set_bit(8, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(63));
+
+    const Bitword< unsafe_bits< uint64_t > > word3{0x0};
+    ASSERT_FALSE(word3.get_next_set_bit(0, &bit_pos));
+    ASSERT_FALSE(word3.get_next_set_bit(8, &bit_pos));
+}
+
+TEST_F(BitwordTest, GetNextResetBit) {
+    uint8_t bit_pos;
+    const Bitword< unsafe_bits< uint64_t > > word1{0x02};
+    ASSERT_TRUE(word1.get_next_reset_bit(0, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(0));
+    ASSERT_TRUE(word1.get_next_reset_bit(1, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(2));
+
+    const Bitword< unsafe_bits< uint64_t > > word2{0x7FFFFFFFFFFFFFFF};
+    ASSERT_TRUE(word2.get_next_reset_bit(0, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(63));
+    ASSERT_TRUE(word2.get_next_reset_bit(8, &bit_pos));
+    ASSERT_EQ(bit_pos, static_cast< uint8_t >(63));
+
+    const Bitword< unsafe_bits< uint64_t > > word3{0xFFFFFFFFFFFFFFFF};
+    ASSERT_FALSE(word3.get_next_reset_bit(0, &bit_pos));
+    ASSERT_FALSE(word3.get_next_reset_bit(8, &bit_pos));
+}
+
+TEST_F(BitwordTest, GetNextResetBits) {
+    uint8_t pcount;
+    const Bitword< unsafe_bits< uint64_t > > word1{0x00};
+    ASSERT_EQ(word1.get_next_reset_bits(0, &pcount), static_cast< uint8_t >(0));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(64));
+    ASSERT_EQ(word1.get_next_reset_bits(8, &pcount), static_cast< uint8_t >(8));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(56));
+
+    const Bitword< unsafe_bits< uint64_t > > word2{0xFFFFFFFFFFFFFF00};
+    ASSERT_EQ(word2.get_next_reset_bits(0, &pcount), static_cast< uint8_t >(0));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(8));
+    ASSERT_EQ(word2.get_next_reset_bits(4, &pcount), static_cast< uint8_t >(4));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(4));
+    ASSERT_EQ(word2.get_next_reset_bits(8, &pcount), static_cast< uint8_t >(64));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(0));
+
+    const Bitword< unsafe_bits< uint64_t > > word3{0x3FFFFFFFFFFFFFFF};
+    ASSERT_EQ(word3.get_next_reset_bits(0, &pcount), static_cast< uint8_t >(62));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(2));
+    ASSERT_EQ(word3.get_next_reset_bits(8, &pcount), static_cast< uint8_t >(62));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(2));
+    ASSERT_EQ(word3.get_next_reset_bits(63, &pcount), static_cast< uint8_t >(63));
+    ASSERT_EQ(pcount, static_cast< uint8_t >(1));
+
+    const Bitword< unsafe_bits< uint64_t > > word4{0xFFFFFFFFFFFFFFFF};
+    ASSERT_EQ(word4.get_next_reset_bits(0, &pcount), static_cast< uint8_t >(64));
+    ASSERT_EQ(word4.get_next_reset_bits(8, &pcount), static_cast< uint8_t >(64));
+}
+
+TEST_F(BitwordTest, SetNextResetBit) {
+    uint8_t pBit;
+    Bitword< unsafe_bits< uint64_t > > word1{0x00};
+    ASSERT_TRUE(word1.set_next_reset_bit(0, 64, &pBit));
+    ASSERT_EQ(pBit, static_cast< uint8_t >(0));
+    ASSERT_TRUE(word1.set_next_reset_bit(1, 64, &pBit));
+    ASSERT_EQ(pBit, static_cast< uint8_t >(1));
+
+    Bitword< unsafe_bits< uint64_t > > word2{0x7FFFFFFFFFFFFFFF};
+    ASSERT_TRUE(word2.set_next_reset_bit(0, 64, &pBit));
+    ASSERT_EQ(pBit, static_cast< uint8_t >(63));
+    ASSERT_FALSE(word2.set_next_reset_bit(1, 64, &pBit));
+
+    Bitword< unsafe_bits< uint64_t > > word3{0x0FF};
+    ASSERT_FALSE(word3.set_next_reset_bit(0, 8, &pBit));
+}
+
+TEST_F(BitwordTest, RightShift) {
+    Bitword< unsafe_bits< uint64_t > > word1{0xFF00};
+    ASSERT_EQ(word1.right_shift(8), static_cast< uint64_t >(0xFF));
+}
+
+TEST_F(BitwordTest, ToString) {
+    const Bitword< unsafe_bits< uint8_t > > word1{0x0F};
+    ASSERT_EQ(word1.to_string(), std::string{"00001111"});
+}
+
+TEST_F(BitwordTest, GetNextResetBitsFiltered)
 {
     ASSERT_TRUE(validate(0xfff0, 0, {5, 5, 1}, 16, bit_match_type::mid_match));
     ASSERT_TRUE(validate(0xfff0, 0, {4, 5, 1}, 0, bit_match_type::lsb_match));
@@ -115,6 +247,39 @@ TEST_F(BitwordTest, TestGetNextResetBitsFiltered)
     ASSERT_TRUE(validate(0x7ff000ffff00ff0f, 0, {11, 11, 1}, 40, bit_match_type::mid_match));
     ASSERT_TRUE(validate(0x7ff000ffff00ff0f, 5, {2, 2, 1}, 5, bit_match_type::mid_match));
     ASSERT_TRUE(validate(0x7ff000ffff00ff0f, 5, {8, 8, 1}, 16, bit_match_type::mid_match));
+}
+
+TEST_F(BitwordTest, GetMaxContiguousResetBits) {
+    uint8_t pmax_count;
+    const Bitword< unsafe_bits< uint64_t > > word1{0xFFFFFFFFFFFFFFFF};
+    ASSERT_EQ(word1.get_max_contiguous_reset_bits(0, &pmax_count), std::numeric_limits<uint8_t>::max());
+
+    const Bitword< unsafe_bits< uint64_t > > word2{0xFFFFFFFFFFFFFFF0};
+    ASSERT_EQ(word2.get_max_contiguous_reset_bits(0, &pmax_count), static_cast< uint8_t >(0));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(4));
+    ASSERT_EQ(word2.get_max_contiguous_reset_bits(1, &pmax_count), static_cast< uint8_t >(1));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(3));
+
+    const Bitword< unsafe_bits< uint64_t > > word3{0x0FFFFFFFFFFFFFFF};
+    ASSERT_EQ(word3.get_max_contiguous_reset_bits(0, &pmax_count), static_cast< uint8_t >(60));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(4));
+    ASSERT_EQ(word3.get_max_contiguous_reset_bits(1, &pmax_count), static_cast< uint8_t >(60));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(4));
+
+    const Bitword< unsafe_bits< uint64_t > > word4{0xFFFFFFFFFFFFFF0F};
+    ASSERT_EQ(word4.get_max_contiguous_reset_bits(0, &pmax_count), static_cast< uint8_t >(4));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(4));
+    ASSERT_EQ(word4.get_max_contiguous_reset_bits(1, &pmax_count), static_cast< uint8_t >(4));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(4));
+    ASSERT_EQ(word4.get_max_contiguous_reset_bits(8, &pmax_count), std::numeric_limits< uint8_t >::max());
+
+    const Bitword< unsafe_bits< uint64_t > > word5{0xFF00FFFFFFFFFF0F};
+    ASSERT_EQ(word5.get_max_contiguous_reset_bits(0, &pmax_count), static_cast< uint8_t >(48));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(8));
+
+    const Bitword< unsafe_bits< uint64_t > > word6{0xFF00FFFFFFFF000F};
+    ASSERT_EQ(word6.get_max_contiguous_reset_bits(0, &pmax_count), static_cast< uint8_t >(4));
+    ASSERT_EQ(pmax_count, static_cast< uint8_t >(12));
 }
 
 int main(int argc, char* argv[]) {
