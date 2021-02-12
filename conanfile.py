7ee1727d57e98e8e45f1dfbff4088701133ed493
Bryan Zimmerman <brzimmerman@ebay.com>
2021-02-12 11:27:30 -0900

    Fix built in functions

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class MetricsConan(ConanFile):
     name = "sisl"
-    version = "5.0.1"
+    version = "5.0.2"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/Symbiosis/sisl"
