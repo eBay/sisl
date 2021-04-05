0c0b6f9813dd32b3d65ad69b622487a59d1f0911
Brian Szmyd <bszmyd@ebay.com>
2021-04-05 21:03:55 +0000

    Revert "Remove override of fmt library"
    This reverts commit 4ebf5e79def535a8c5c939544662d2e67be6c9cf.

--- conanfile.py
+++ conanfile.py
@@ -37,6 +37,7 @@ class MetricsConan(ConanFile):
                     "evhtp/1.2.18.2",
                     "snappy/1.1.8",
                     "flatbuffers/1.11.0",
+                    ("fmt/7.1.3", "override"),
                     "folly/2020.05.04.00",
                     "nlohmann_json/3.8.0",
                     ("openssl/1.1.1g", "override"),
