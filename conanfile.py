040257bfccf203f456ea5ba90269052775581834
Brian Szmyd <bszmyd@ebay.com>
2021-04-05 21:25:02 +0000

    Fix UT with FMT 7.1.x

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class MetricsConan(ConanFile):
     name = "sisl"
-    version = "4.4.7"
+    version = "4.4.8"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/Symbiosis/sisl"
