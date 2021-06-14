df7818bfc1e907cfe75865a17ebb51dc28bb3b7f
Bryan Zimmerman <brzimmerman@ebay.com>
2021-06-14 17:42:19 -0400

    SDSTOR-4706: Bump version

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import os
 
 class MetricsConan(ConanFile):
     name = "sisl"
-    version = "5.0.4"
+    version = "5.0.5"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/Symbiosis/sisl"
