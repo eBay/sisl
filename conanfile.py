a4e1cef82e995f531da9d843e1b8bb267a058a0c
Bryan Zimmerman <brzimmerman@ebay.com>
2021-06-28 14:02:23 -0400

    Fix bug where ThreadBufferMetricsGroup destructor was not in cpp to allow std::unique_ptrs to have full scope definition Proper initialization of memory

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import os
 
 class MetricsConan(ConanFile):
     name = "sisl"
-    version = "5.0.6"
+    version = "5.0.7"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/Symbiosis/sisl"
