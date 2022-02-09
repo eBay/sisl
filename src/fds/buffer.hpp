644831f0d148f51469b66b5130d99c0a60b843bd
dachau <dachau@ebay.com>
2022-02-09 14:40:56 -0700

    SDSTOR-3661: Remove implementation in header file

--- src/fds/buffer.hpp
+++ src/fds/buffer.hpp
@@ -101,10 +101,8 @@ public:
         aligned_free(p, tag);
     }
 
-    virtual void* aligned_pool_alloc(const size_t align, const size_t sz, const sisl::buftag tag) {
-        return nullptr;
-    };
-    virtual void aligned_pool_free(uint8_t* const b, const size_t sz, const sisl::buftag tag) {};
+    virtual void* aligned_pool_alloc(const size_t align, const size_t sz, const sisl::buftag tag);
+    virtual void aligned_pool_free(uint8_t* const b, const size_t sz, const sisl::buftag tag);
 
     virtual size_t buf_size(uint8_t* buf) const {
 #ifdef __linux__
