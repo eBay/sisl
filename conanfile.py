b71371db74675c2d23efcc7e4ab9bbe893fae341
Brian Szmyd <bszmyd@ebay.com>
2022-08-03 11:25:16 -0700

    Recipe cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from conans import ConanFile, CMake, tools
 import os
 
 class MetricsConan(ConanFile):
     name = "sisl"
     version = "8.0.1"
-
-    license = "Apache"
-    url = "https://github.corp.ebay.com/Symbiosis/sisl"
+    homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
-    revision_mode = "scm"
+    topics = ("ebay", "components", "core", "efficiency")
+    url = "https://github.com/eBay/sisl"
+    license = "Apache-2.0"
 
     settings = "arch", "os", "compiler", "build_type"
     options = {
@@ -22,37 +20,15 @@ class MetricsConan(ConanFile):
                 'prerelease' : ['True', 'False'],
                 'with_evhtp' : ['True', 'False'],
               }
-    default_options = (
-                        'shared=False',
-                        'fPIC=True',
-                        'coverage=False',
-                        'sanitize=False',
-                        'malloc_impl=libc',
-                        'prerelease=True',
-                        'with_evhtp=False',
-                        )
-
-    requires = (
-                    # Custom packages
-                    "prometheus-cpp/1.0.0",
-
-                    # Generic packages (conan-center)
-                    "boost/1.79.0",
-                    "cpr/1.8.1",
-                    "cxxopts/2.2.1",
-                    "flatbuffers/1.12.0",
-                    "folly/2022.01.31.00",
-                    "jwt-cpp/0.4.0",
-                    "nlohmann_json/3.10.5",
-                    "semver.c/1.0.0",
-                    "spdlog/1.10.0",
-                    "userspace-rcu/0.11.4",
-                    ("fmt/8.1.1", "override"),
-                    ("libevent/2.1.12", "override"),
-                    ("openssl/1.1.1q", "override"),
-                    ("xz_utils/5.2.5", "override"),
-                    ("zlib/1.2.12", "override"),
-                )
+    default_options = {
+                'shared': False,
+                'fPIC': True,
+                'coverage': False,
+                'sanitize': False,
+                'malloc_impl': 'libc',
+                'prerelease': True,
+                'with_evhtp': False,
+            }
 
     build_requires = (
                     # Generic packages (conan-center)
@@ -60,8 +36,8 @@ class MetricsConan(ConanFile):
                     "gtest/1.11.0",
                 )
 
-    generators = "cmake", "cmake_find_package_multi"
-    exports_sources = ("CMakeLists.txt", "cmake/*", "src/*")
+    generators = "cmake", "cmake_find_package"
+    exports_sources = ("CMakeLists.txt", "cmake/*", "src/*", "LICENSE")
 
     def config_options(self):
         if self.settings.build_type != "Debug":
@@ -80,6 +56,25 @@ class MetricsConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
+            # Custom packages
+        self.requires("prometheus-cpp/1.0.0")
+
+        # Generic packages (conan-center)
+        self.requires("boost/1.79.0")
+        self.requires("cpr/1.8.1")
+        self.requires("cxxopts/2.2.1")
+        self.requires("flatbuffers/1.12.0")
+        self.requires("folly/2022.01.31.00")
+        self.requires("jwt-cpp/0.4.0")
+        self.requires("nlohmann_json/3.10.5")
+        self.requires("semver.c/1.0.0")
+        self.requires("spdlog/1.10.0")
+        self.requires("userspace-rcu/0.11.4")
+        self.requires("fmt/8.1.1",          override=True)
+        self.requires("libevent/2.1.12",    override=True)
+        self.requires("openssl/1.1.1q",     override=True)
+        self.requires("xz_utils/5.2.5",     override=True)
+        self.requires("zlib/1.2.12",        override=True)
         if self.options.malloc_impl == "jemalloc":
             self.requires("jemalloc/5.2.1")
         elif self.options.malloc_impl == "tcmalloc":
@@ -113,6 +108,7 @@ class MetricsConan(ConanFile):
         cmake.test(target=test_target)
 
     def package(self):
+        self.copy(pattern="LICENSE*", dst="licenses")
         self.copy("*.hpp", src="src/", dst="include/sisl", keep_path=True)
         self.copy("*.h", src="src/", dst="include/sisl", keep_path=True)
         self.copy("*.a", dst="lib/", keep_path=False)
@@ -123,7 +119,7 @@ class MetricsConan(ConanFile):
         self.copy("*.cmake", dst="cmake/", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["sisl"]
         self.cpp_info.cppflags.append("-Wno-unused-local-typedefs")
         self.cpp_info.cppflags.append("-fconcepts")
         self.cpp_info.includedirs = ["include", "include/sisl/"]
