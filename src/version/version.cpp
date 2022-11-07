119faf69374ff1572989489f1006f6eb84a3745a
Brian Szmyd <bszmyd@ebay.com>
2022-11-07 12:17:48 -0700

    Standardize version library.

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -14,7 +14,7 @@
  * specific language governing permissions and limitations under the License.
  *
  *********************************************************************************/
-#include "version.hpp"
+#include <sisl/version.hpp>
 #include <cassert>
 
 namespace sisl {
