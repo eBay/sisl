3bb3cac37623c3d51e5c2d539588eea43a66037e
Ravi Akella email = raakella@ebay.com <raakella@sdsbuild07>
2023-01-22 20:59:37 -0700

    resolve declaration error

--- src/file_watcher/file_watcher_test.cpp
+++ src/file_watcher/file_watcher_test.cpp
@@ -77,7 +77,7 @@ TEST_F(FileWatcherTest, basic_watcher) {
     {
         auto lk = std::unique_lock< std::mutex >(m_file_change_params.file_change_lock);
         EXPECT_TRUE(m_file_change_params.file_change_cv.wait_for(
-            lk, std::chrono::milliseconds(500), [this]() { return m_file_change_params.cb_call_count == 0; }));
+            lk, std::chrono::milliseconds(1500), [this]() { return m_file_change_params.cb_call_count == 0; }));
         EXPECT_FALSE(m_file_change_params.is_deleted);
         m_file_change_params.cb_call_count = 1; // set it 1 for the next iteration of the test
     }
@@ -87,7 +87,7 @@ TEST_F(FileWatcherTest, basic_watcher) {
     {
         auto lk = std::unique_lock< std::mutex >(m_file_change_params.file_change_lock);
         EXPECT_TRUE(m_file_change_params.file_change_cv.wait_for(
-            lk, std::chrono::milliseconds(500), [this]() { return m_file_change_params.cb_call_count == 0; }));
+            lk, std::chrono::milliseconds(1500), [this]() { return m_file_change_params.cb_call_count == 0; }));
         EXPECT_TRUE(m_file_change_params.is_deleted);
         m_file_change_params.cb_call_count = 1; // set it 1 for the next iteration of the test
     }
@@ -98,7 +98,7 @@ TEST_F(FileWatcherTest, basic_watcher) {
     {
         auto lk = std::unique_lock< std::mutex >(m_file_change_params.file_change_lock);
         EXPECT_TRUE(m_file_change_params.file_change_cv.wait_for(
-            lk, std::chrono::milliseconds(500), [this]() { return m_file_change_params.cb_call_count == 0; }));
+            lk, std::chrono::milliseconds(1500), [this]() { return m_file_change_params.cb_call_count == 0; }));
         EXPECT_FALSE(m_file_change_params.is_deleted);
     }
 }
@@ -121,7 +121,7 @@ TEST_F(FileWatcherTest, multiple_watchers) {
     {
         auto lk = std::unique_lock< std::mutex >(m_file_change_params.file_change_lock);
         EXPECT_TRUE(m_file_change_params.file_change_cv.wait_for(
-            lk, std::chrono::milliseconds(500), [this]() { return m_file_change_params.cb_call_count == 0; }));
+            lk, std::chrono::milliseconds(1500), [this]() { return m_file_change_params.cb_call_count == 0; }));
         EXPECT_FALSE(m_file_change_params.is_deleted);
     }
 }
