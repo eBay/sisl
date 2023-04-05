7f609bf2497b5935d25798a926255bfcec0b48fe
Kadayam, Harihara(hkadayam) <hkadayam@ebay.com>
2023-04-04 20:22:47 -0700

    Create a link called 'latest' to the last logdir

--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -138,18 +138,30 @@ std::shared_ptr< spdlog::logger >& GetCriticalLogger() {
 }
 
 static std::filesystem::path get_base_dir() {
-    const auto cwd{std::filesystem::current_path()};
-    auto p{cwd / "logs"};
+    namespace fs = std::filesystem;
+    const auto cwd = fs::current_path();
+    const auto log_dir{cwd / "logs"};
+
     // Construct a unique directory path based on the current time
     auto const current_time{std::chrono::system_clock::now()};
     auto const current_t{std::chrono::system_clock::to_time_t(current_time)};
     auto const current_tm{std::localtime(&current_t)};
     std::array< char, PATH_MAX > c_time;
     if (std::strftime(c_time.data(), c_time.size(), "%F_%R", current_tm)) {
-        p /= c_time.data();
-        std::filesystem::create_directories(p);
+        const fs::path cur_log_dir = log_dir / c_time.data();
+        fs::create_directories(cur_log_dir);
+
+        const fs::path sym_path = log_dir / "latest";
+        try {
+            if (fs::is_symlink(sym_path)) { fs::remove(sym_path); }
+            fs::create_directory_symlink(cur_log_dir, sym_path);
+        } catch (std::exception& e) {
+            LOGINFO("Unable to create latest symlink={} to log dir={}, ignoring symlink creation\n", sym_path, log_dir);
+        }
+        return cur_log_dir;
+    } else {
+        return log_dir;
     }
-    return p;
 }
 
 static std::filesystem::path log_path(std::string const& name) {
