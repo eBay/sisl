107eb538f604973c308fa67db549c5c9e0e86348
Brian Szmyd <bszmyd@ebay.com>
2023-05-04 09:19:44 -0700

    v8.4.1 - Added support for jThread, default build on gcc11

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,5 +1,6 @@
 #include <sisl/logging/logging.h>
 #include <sisl/options/options.h>
+#include <sisl/utility/thread_factory.hpp>
 
 SISL_LOGGING_INIT(my_module)
 
@@ -7,22 +8,37 @@ SISL_OPTIONS_ENABLE(logging)
 
 extern void example_decl();
 
+using namespace std::chrono_literals;
+
 int main(int argc, char** argv) {
-   SISL_OPTIONS_LOAD(argc, argv, logging)
-   sisl::logging::SetLogger(std::string(argv[0]));
-   spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
+    SISL_OPTIONS_LOAD(argc, argv, logging)
+    sisl::logging::SetLogger(std::string(argv[0]));
+    spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
 
-   LOGTRACE("Trace");
-   LOGDEBUG("Debug");
-   LOGINFO("Info");
-   LOGWARN("Warning");
-   LOGERROR("Error");
-   LOGCRITICAL("Critical");
+    LOGTRACE("Trace");
+    LOGDEBUG("Debug");
+    LOGINFO("Info");
+    LOGWARN("Warning");
+    LOGERROR("Error");
+    LOGCRITICAL("Critical");
 
-   example_decl();
+    auto thread = sisl::named_jthread("example_decl", [](std::stop_token stoken) {
+        example_decl();
+        while (!stoken.stop_requested()) {
+            LOGWARNMOD(my_module, "Sleeping...");
+            std::this_thread::sleep_for(1500ms);
+        }
+        LOGINFOMOD(my_module, "Waking...");
+        std::this_thread::sleep_for(1500ms);
+    });
+    std::this_thread::sleep_for(300ms);
+    auto stop_source = thread.get_stop_source();
 
-   auto custom_logger = sisl::logging::CreateCustomLogger("test_package", "_custom", false /*stdout*/, true /*stderr*/);
-   LOGINFOMOD_USING_LOGGER(my_module, custom_logger, "hello world");
-   DEBUG_ASSERT(true, "Always True");
-   return 0;
+    auto custom_logger =
+        sisl::logging::CreateCustomLogger("test_package", "_custom", false /*stdout*/, true /*stderr*/);
+    LOGINFOMOD_USING_LOGGER(my_module, custom_logger, "hello world");
+    DEBUG_ASSERT(true, "Always True");
+    RELEASE_ASSERT(stop_source.request_stop(), "Should be!");
+
+    return 0;
 }
