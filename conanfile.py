e54738fbc59129cbab5308d8270b50992cd3c419
Sanal P <sanebay3@gmail.com>
2023-06-13 15:06:57 -0700

    Fix build for sisl 8.5.3

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ required_conan_version = ">=1.50.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "8.5.3"
+    version = "8.5.4"
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
     topics = ("ebay", "components", "core", "efficiency")
