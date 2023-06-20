2e00a8ae2b41c20a72dd91daca26d10c2f25c350
Brian Szmyd <bszmyd@ebay.com>
2023-06-20 12:41:13 -0700

    Adding breakpad library.

--- src/logging/stacktrace.cpp
+++ src/logging/stacktrace.cpp
@@ -33,11 +33,7 @@
 #endif
 
 #include <sisl/logging/logging.h>
-#include "backtrace.h"
-
-namespace {
-constexpr uint64_t backtrace_timeout_ms{4 * backtrace_detail::pipe_timeout_ms};
-}
+//#include "backtrace.h"
 
 namespace sisl {
 namespace logging {
@@ -45,10 +41,8 @@ static bool g_custom_signal_handler_installed{false};
 static size_t g_custom_signal_handlers{0};
 static bool g_crash_handle_all_threads{true};
 static std::mutex g_mtx_stack_dump_outstanding;
-static size_t g_stack_dump_outstanding{0};
 static std::condition_variable g_stack_dump_cv;
 static std::mutex g_hdlr_mutex;
-static std::array< char, max_stacktrace_size() > g_stacktrace_buff;
 
 typedef struct SignalHandlerData {
     SignalHandlerData(std::string name, const sig_handler_t handler) : name{std::move(name)}, handler{handler} {}
@@ -171,107 +165,6 @@ static void sigint_handler(const SignalType signal_number) {
 }
 
 static void bt_dumper([[maybe_unused]] const SignalType signal_number) {
-    g_stacktrace_buff.fill(0);
-    stack_backtrace(g_stacktrace_buff.data(), g_stacktrace_buff.size(), true);
-    bool notify{false};
-    {
-        std::unique_lock lock{g_mtx_stack_dump_outstanding};
-        if (g_stack_dump_outstanding > 0) {
-            --g_stack_dump_outstanding;
-            notify = true;
-        }
-    }
-    if (notify) g_stack_dump_cv.notify_all();
-}
-
-static void log_stack_trace_all_threads() {
-    std::unique_lock logger_lock{LoggerThreadContext::s_logger_thread_mutex};
-    auto& logger{GetLogger()};
-    auto& critical_logger{GetCriticalLogger()};
-    size_t thread_count{1};
-
-    const auto dump_thread{[&logger, &critical_logger, &thread_count](const bool signal_thread, const auto thread_id) {
-        if (signal_thread) {
-            const auto log_failure{[&logger, &critical_logger, &thread_count, &thread_id](const char* const msg) {
-                if (logger) {
-#ifndef __APPLE__
-                    logger->critical("Thread ID: {}, Thread num: {} - {}\n", thread_id, thread_count, msg);
-#else
-                    logger->critical("Thread num: {} - {}\n", thread_count, msg);
-#endif
-                    logger->flush();
-                }
-                if (critical_logger) {
-#ifndef __APPLE__
-                    critical_logger->critical("Thread ID: {}, Thread num: {} - {}\n", thread_id, thread_count, msg);
-#else
-                    critical_logger->critical("Thread num: {} - {}\n", thread_count, msg);
-#endif
-                    critical_logger->flush();
-                }
-            }};
-
-            {
-                std::unique_lock outstanding_lock{g_mtx_stack_dump_outstanding};
-                assert(g_stack_dump_outstanding == 0);
-                g_stack_dump_outstanding = 1;
-            }
-            if (!send_thread_signal(thread_id, SIGUSR3)) {
-                {
-                    std::unique_lock outstanding_lock{g_mtx_stack_dump_outstanding};
-                    g_stack_dump_outstanding = 0;
-                }
-                log_failure("Invalid/terminated thread");
-                return;
-            }
-
-            {
-                std::unique_lock outstanding_lock{g_mtx_stack_dump_outstanding};
-                const auto result{g_stack_dump_cv.wait_for(outstanding_lock,
-                                                           std::chrono::milliseconds{backtrace_timeout_ms},
-                                                           [] { return (g_stack_dump_outstanding == 0); })};
-                if (!result) {
-                    g_stack_dump_outstanding = 0;
-                    outstanding_lock.unlock();
-                    log_failure("Timeout waiting for stacktrace");
-                    return;
-                }
-            }
-        } else {
-            // dump the thread without recursive signal
-            g_stacktrace_buff.fill(0);
-            stack_backtrace(g_stacktrace_buff.data(), g_stacktrace_buff.size(), true);
-        }
-
-        if (logger) {
-#ifndef __APPLE__
-            logger->critical("Thread ID: {}, Thread num: {}\n{}", thread_id, thread_count, g_stacktrace_buff.data());
-#else
-            logger->critical("Thread num: {}\n{}", thread_count, g_stacktrace_buff.data());
-#endif
-            logger->flush();
-        }
-        if (critical_logger) {
-#ifndef __APPLE__
-            critical_logger->critical("Thread ID: {}, Thread num: {}\n{}", thread_id, thread_count,
-                                      g_stacktrace_buff.data());
-#else
-            critical_logger->critical("Thread num: {}\n{}", thread_count, g_stacktrace_buff.data());
-#endif
-            critical_logger->flush();
-        }
-    }};
-
-    // First dump this thread context
-    dump_thread(false, logger_thread_ctx.m_thread_id);
-    ++thread_count;
-
-    // dump other threads
-    for (auto* const ctx : LoggerThreadContext::s_logger_thread_set) {
-        if (ctx == &logger_thread_ctx) { continue; }
-        dump_thread(true, ctx->m_thread_id);
-        ++thread_count;
-    }
 }
 
 /************************************************* Exported APIs **********************************/
@@ -431,16 +324,17 @@ void log_custom_signal_handlers() {
     LOGINFO("Custom Signal handlers: {}", m);
 }
 
-void log_stack_trace(const bool all_threads) {
-    if (is_crash_handler_installed() && all_threads) {
-        log_stack_trace_all_threads();
-    } else {
+void log_stack_trace([[maybe_unused]] const bool all_threads) {
+    //if (is_crash_handler_installed() && all_threads) {
+    //    log_stack_trace_all_threads();
+    //} else {
+        //TODO
         // make this static so that no memory allocation is necessary
-        static std::array< char, max_stacktrace_size() > buff;
-        buff.fill(0);
-        [[maybe_unused]] const size_t s{stack_backtrace(buff.data(), buff.size(), true)};
-        LOGCRITICAL("\n\n{}", buff.data());
-    }
+        //static std::array< char, max_stacktrace_size() > buff;
+        //buff.fill(0);
+        //[[maybe_unused]] const size_t s{stack_backtrace(buff.data(), buff.size(), true)};
+        //LOGCRITICAL("\n\n{}", buff.data());
+    //}
 }
 
 bool send_thread_signal(const pthread_t thr, const SignalType sig_num) { return (::pthread_kill(thr, sig_num) == 0); }
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,7 @@
 #include <sisl/logging/logging.h>
 #include <sisl/options/options.h>
 #include <sisl/utility/thread_factory.hpp>
+#include <breakpad/client/linux/handler/exception_handler.h>
 
 SISL_LOGGING_INIT(my_module)
 
@@ -10,7 +11,19 @@ extern void example_decl();
 
 using namespace std::chrono_literals;
 
+static bool dumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+void* context, bool succeeded) {
+  printf("Dump path: %s\n", descriptor.path());
+  return succeeded;
+}
+
+[[ maybe_unused ]]
+void crash() { volatile int* a = (int*)(NULL); *a = 1; }
+
 int main(int argc, char** argv) {
+    google_breakpad::MinidumpDescriptor descriptor("./");
+    google_breakpad::ExceptionHandler eh(descriptor, NULL, dumpCallback, NULL, true, -1);
+
     SISL_OPTIONS_LOAD(argc, argv, logging)
     sisl::logging::SetLogger(std::string(argv[0]));
     spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
@@ -36,6 +49,6 @@ int main(int argc, char** argv) {
     LOGINFOMOD_USING_LOGGER(my_module, custom_logger, "hello world");
     DEBUG_ASSERT(true, "Always True");
     _thread.join();
-
+    //crash();
     return 0;
 }
