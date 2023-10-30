b45862d826b7781b965e77b1a760747b58e25984
Harihara Kadayam <harihara.kadayam@gmail.com>
2023-10-30 16:11:55 -0700

    New concurrent insertable vector (#188)

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ required_conan_version = ">=1.52.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "10.2.2"
+    version = "10.2.3"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
