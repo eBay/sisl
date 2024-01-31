20d98afc8ec1d54a7735d52e94907209d114e932
Brian Szmyd <bszmyd@ebay.com>
2024-01-31 10:01:04 -0700

    Lift test package for 2.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,19 +1,19 @@
 from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
-    def generate(self):
-        # This generates "conan_toolchain.cmake" in self.generators_folder
-        tc = CMakeToolchain(self)
-        tc.generate()
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
-        # This generates "boost-config.cmake" and "grpc-config.cmake" etc in self.generators_folder
-        deps = CMakeDeps(self)
-        deps.generate()
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -21,6 +21,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
