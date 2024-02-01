bc3113599231ccc87186e810a430253b5f4eecfe
Brian Szmyd <bszmyd@ebay.com>
2024-02-01 12:21:35 -0700

    Split package into components.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ required_conan_version = ">=1.60.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "11.0.6"
+    version = "11.1.1"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
@@ -22,7 +22,6 @@ class SISLConan(ConanFile):
                 "shared": ['True', 'False'],
                 "fPIC": ['True', 'False'],
                 "coverage": ['True', 'False'],
-                'testing' : ['True', 'False'],
                 "sanitize": ['True', 'False'],
                 'prerelease' : ['True', 'False'],
                 'malloc_impl' : ['libc', 'tcmalloc', 'jemalloc'],
@@ -31,34 +30,36 @@ class SISLConan(ConanFile):
                 'shared': False,
                 'fPIC': True,
                 'coverage': False,
-                'testing': True,
                 'sanitize': False,
                 'prerelease': False,
                 'malloc_impl': 'libc',
             }
 
-    exports = ["LICENSE"]
     exports_sources = (
+                "LICENSE",
                 "CMakeLists.txt",
                 "cmake/*",
                 "include/*",
                 "src/*",
             )
 
+    def _min_cppstd(self):
+        return 20
+
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, 20)
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
 
     def configure(self):
         if self.settings.compiler in ["gcc"]:
             self.options['pistache'].with_ssl: True
         if self.options.shared:
-            self.options.safe_remove("fPIC")
+            self.options.rm_safe("fPIC")
         if self.settings.build_type == "Debug":
-            self.options["sisl/*"].prerelease = True
+            self.options.rm_safe("prerelease")
             if self.options.coverage and self.options.sanitize:
                 raise ConanInvalidConfiguration("Sanitizer does not work with Code Coverage!")
-            if not self.options.testing:
+            if not self.conf.get("tools.build:skip_test", default=False):
                 if self.options.coverage or self.options.sanitize:
                     raise ConanInvalidConfiguration("Coverage/Sanitizer requires Testing!")
 
@@ -106,10 +107,13 @@ class SISLConan(ConanFile):
         tc.variables['MALLOC_IMPL'] = self.options.malloc_impl
         tc.variables["ENABLE_TESTING"] = 'ON'
         tc.variables["PACKAGE_VERSION"] = self.version
+        if self.options.get_safe("prerelease"):
+            tc.preprocessor_definitions["_PRERELEASE"] = "1"
         if self.settings.build_type == "Debug":
-            if self.options.coverage:
+            tc.preprocessor_definitions["_PRERELEASE"] = "1"
+            if self.options.get_safe("coverage"):
                 tc.variables['BUILD_COVERAGE'] = 'ON'
-            elif self.options.sanitize:
+            elif self.options.get_safe("sanitize"):
                 tc.variables['MEMORY_SANITIZER_ON'] = 'ON'
         tc.generate()
 
@@ -144,26 +148,42 @@ class SISLConan(ConanFile):
         copy(self, "*security_config_generated.h", join(self.build_folder, "src"), gen_dir, keep_path=True)
         copy(self, "settings_gen.cmake", join(self.source_folder, "cmake"), join(self.package_folder, "cmake"), keep_path=False)
 
+    def _add_component(self, lib, requirelist=[]):
+        self.cpp_info.components[lib].libs = [lib]
+        self.cpp_info.components[lib].set_property("pkg_config_name", f"lib{lib}")
+        self.cpp_info.components[lib].requires.extend(requirelist)
+
     def package_info(self):
-        self.cpp_info.libs = ["sisl"]
+        self._add_component("sisl")
+        self._add_component("flip")
 
-        if self.options.prerelease:
-            self.cpp_info.cxxflags.append("-D_PRERELEASE=1")
-
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.append("flip")
-            self.cpp_info.cppflags.append("-D_POSIX_C_SOURCE=200809L")
-            self.cpp_info.cppflags.append("-D_FILE_OFFSET_BITS=64")
-            self.cpp_info.cppflags.append("-D_LARGEFILE64")
-            self.cpp_info.system_libs.extend(["dl", "pthread"])
-            self.cpp_info.exelinkflags.extend(["-export-dynamic"])
-
-        if  self.options.sanitize:
-            self.cpp_info.sharedlinkflags.append("-fsanitize=address")
-            self.cpp_info.exelinkflags.append("-fsanitize=address")
-            self.cpp_info.sharedlinkflags.append("-fsanitize=undefined")
-            self.cpp_info.exelinkflags.append("-fsanitize=undefined")
-        if self.options.malloc_impl == 'jemalloc':
-            self.cpp_info.cppflags.append("-DUSE_JEMALLOC=1")
-        elif self.options.malloc_impl == 'tcmalloc':
-            self.cpp_info.cppflags.append("-DUSING_TCMALLOC=1")
+        for component in self.cpp_info.components.values():
+            component.requires.extend([
+                "boost::boost",
+                "breakpad::breakpad",
+                "cxxopts::cxxopts",
+                "folly::folly",
+                "spdlog::spdlog",
+                "grpc::grpc++",
+                "nlohmann_json::nlohmann_json",
+                "prometheus-cpp::prometheus-cpp",
+                "userspace-rcu::userspace-rcu",
+                "zmarok-semver::zmarok-semver",
+                ])
+            if self.settings.os in ["Linux", "FreeBSD"]:
+                component.defines.append("_POSIX_C_SOURCE=200809L")
+                component.defines.append("_FILE_OFFSET_BITS=64")
+                component.defines.append("_LARGEFILE64")
+                component.system_libs.extend(["dl", "pthread"])
+                component.exelinkflags.extend(["-export-dynamic"])
+            if self.options.get_safe("prerelease"):
+                component.defines.append("_PRERELEASE=1")
+            if  self.options.get_safe("sanitize"):
+                component.sharedlinkflags.append("-fsanitize=address")
+                component.exelinkflags.append("-fsanitize=address")
+                component.sharedlinkflags.append("-fsanitize=undefined")
+                component.exelinkflags.append("-fsanitize=undefined")
+            if self.options.malloc_impl == 'jemalloc':
+                self.cpp_info.defines.append("USE_JEMALLOC=1")
+            elif self.options.malloc_impl == 'tcmalloc':
+                self.cpp_info.defines.append("USING_TCMALLOC=1")
