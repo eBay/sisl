c45144d4856bdf6a90f6b90028784455862503cb
Harihara Kadayam <harihara.kadayam@gmail.com>
2024-03-08 12:00:35 -0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "11.1.8"
+    version = "11.1.9"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
