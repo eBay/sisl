370c772ab1a894c500fb7f53812951d6f4988e57
Xiaoxi Chen <xiaoxchen@ebay.com>
2024-03-27 11:29:47 +0800

    Add max_receive_msg_size parameters. (#230)
    max_receive_msg_size default to 4M, which might be too small
    for some usecases, allow this to be reconfigurable.
    
    Signed-off-by: Xiaoxi Chen <xiaoxchen@ebay.com>

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "12.0.1"
+    version = "12.1.1"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
