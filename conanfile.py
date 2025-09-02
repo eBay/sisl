93105570dfbf2102c2f9ff6706a7fdba6f068a4e
Xiaoxi Chen <xiaoxchen@ebay.com>
2025-09-02 15:54:03 +0800

    Add PercentileBuckets Bucket
    Signed-off-by: Xiaoxi Chen <xiaoxchen@ebay.com>

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "12.4.6"
+    version = "12.4.7"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
