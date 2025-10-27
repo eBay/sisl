caced6ed2d37ce290f1768e5ce0b6373c853a7f7
Brian Szmyd <bszmyd@ebay.com>
2025-10-27 16:38:17 -0700

    Downrev folly for ThreadLocal issues.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class SISLConan(ConanFile):
     name = "sisl"
-    version = "13.0.1"
+    version = "13.0.2"
 
     homepage = "https://github.com/eBay/sisl"
     description = "Library for fast data structures, utilities"
@@ -90,7 +90,7 @@ class SISLConan(ConanFile):
 
         if self.options.metrics:
             self.requires("flatbuffers/24.12.23", transitive_headers=True)
-            self.requires("folly/nu2.2024.08.12.00", transitive_headers=True)
+            self.requires("folly/nu2.2023.12.18.00", transitive_headers=True)
             self.requires("prometheus-cpp/1.1.0", transitive_headers=True)
             self.requires("snappy/[^1.2]", transitive_headers=True)
             self.requires("userspace-rcu/nu2.0.14.0", transitive_headers=True)
